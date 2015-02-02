d1f52f0c197f2092cd873aeb5b0c3c807d2e0b66
levi <velocorner.com@gmail.com>
2015-02-03 00:08:33 +0100

    some exercises

--- common/src/test/scala/spark/WordCount.scala
+++ common/src/test/scala/spark/WordCount.scala
@@ -60,3 +60,26 @@ object WordCount extends App {
     //wc.saveAsTextFile(out)
   }
 }
+
+// Exercise: Use other versions of the Bible:
+//   The data directory contains similar files for the Tanach (t3utf.dat - in Hebrew),
+//   the Latin Vulgate (vuldat.txt), the Septuagint (sept.txt - Greek)
+// Exercise: See the Scaladoc page for `OrderedRDDFunctions`:
+//   http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.rdd.OrderedRDDFunctions
+//   Sort the output by word, try both ascending and descending.
+//   Note this can be expensive for large data sets!
+// Exercise: Take the output from the previous exercise and count the number
+//   of words that start with each letter of the alphabet and each digit.
+// Exercise (Hard): Sort the output by count. You can't use the same
+//   approach as in the previous exercise. Hint: See RDD.keyBy
+//   (http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.rdd.RDD)
+//   What's the most frequent word that isn't a "stop word".
+// Exercise (Hard): Group the word-count pairs by count. In other words,
+//   All pairs where the count is 1 are together (i.e., just one occurrence
+//   of those words was found), all pairs where the count is 2, etc. Sort
+//   ascending or descending. Hint: Is there a method for grouping?
+// Exercise (Thought Experiment): Consider the size of each group created
+//   in the previous exercise and the distribution of those sizes vs. counts.
+//   What characteristics would you expect for this distribution? That is,
+//   which words (or kinds of words) would you expect to occur most
+//   frequently? What kind of distribution fits the counts (numbers)?
