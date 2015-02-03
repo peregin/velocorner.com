7a8d44dda6b977ef3627242a2a0d4e883742ea18
levi <velocorner.com@gmail.com>
2015-02-03 20:21:58 +0100

    renaming

--- project/build.scala
+++ project/build.scala
@@ -3,42 +3,38 @@ import Keys._
 
 
 object dependency {
-  object Version {
-    val Spark        = "1.2.0"
-    val ScalaTest    = "2.1.4"
-    val ScalaCheck   = "1.11.3"
-  }
 
-  val sparkCore      = "org.apache.spark"  %% "spark-core"      % Version.Spark
-  val sparkStreaming = "org.apache.spark"  %% "spark-streaming" % Version.Spark
-  val sparkSQL       = "org.apache.spark"  %% "spark-sql"       % Version.Spark
+  val SparkVersion = "1.2.0"
 
-  val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.ScalaTest  % "test"
-  val scalaCheck     = "org.scalacheck"    %% "scalacheck"      % Version.ScalaCheck % "test"
+  val sparkCore = "org.apache.spark" %% "spark-core" % SparkVersion
+  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % SparkVersion
+  val sparkSQL = "org.apache.spark" %% "spark-sql" % SparkVersion
+
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.1.4" % "test"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.11.3" % "test"
 }
 
 object dependencies {
   import dependency._
 
-  def spark = Seq(sparkCore, sparkStreaming, sparkSQL,
-    scalaTest, scalaCheck)
+  def spark = Seq(sparkCore, sparkStreaming, sparkSQL, scalaTest, scalaCheck)
 }
 
 object build extends Build {
 
 
   lazy val buildSettings = Defaults.coreDefaultSettings ++ Seq (
-    name          := "velocorner",
-    version       := "1.0.0-SNAPSHOT",
-    scalaVersion  := "2.10.4",
-    organization  := "com.github.peregin",
-    description   := "The Cycling Platform",
+    name := "velocorner",
+    version := "1.0.0-SNAPSHOT",
+    scalaVersion := "2.10.4",
+    organization := "com.github.peregin",
+    description := "The Cycling Platform",
     scalacOptions := Seq("-deprecation", "-feature", "-unchecked", "-encoding", "utf8")
   )
 
   lazy val common = Project(
-    id = "common",
-    base = file("common"),
+    id = "data-feed",
+    base = file("data-feed"),
     settings = buildSettings ++ Seq(
       libraryDependencies ++= dependencies.spark
     )
