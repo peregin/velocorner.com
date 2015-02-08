a3d6767c14bc0a7975df6cc86dd55f46aa76a46d
levi <velocorner.com@gmail.com>
2015-02-08 12:08:59 +0100

    split data cruncher and storage layers

--- project/build.scala
+++ project/build.scala
@@ -6,12 +6,12 @@ import PlayKeys._
 
 object dependencies {
 
-  val SparkVersion = "1.2.0"
 
   val couchbaseClient = "com.couchbase.client" % "couchbase-client" % "1.4.7"
 
   val json = "com.typesafe.play" %% "play-json" % play.core.PlayVersion.current
 
+  val SparkVersion = "1.2.0"
   val sparkCore = "org.apache.spark" %% "spark-core" % SparkVersion
   val sparkStreaming = "org.apache.spark" %% "spark-streaming" % SparkVersion
   val sparkSQL = "org.apache.spark" %% "spark-sql" % SparkVersion
@@ -27,7 +27,6 @@ object build extends Build {
 
 
   lazy val buildSettings = Defaults.coreDefaultSettings ++ Seq (
-    name := "velocorner",
     version := "1.0.0-SNAPSHOT",
     scalaVersion := "2.10.4",
     organization := "com.github.peregin",
@@ -35,30 +34,37 @@ object build extends Build {
     scalacOptions := Seq("-deprecation", "-feature", "-unchecked", "-encoding", "utf8")
   )
 
-  lazy val dataCruncher = Project(
-    id = "data-cruncher",
-    base = file("data-cruncher"),
+  lazy val dataStorage = Project(
+    id = "data-storage",
+    base = file("data-storage"),
     settings = buildSettings ++ Seq(
-      libraryDependencies ++= dependencies.spark ++ Seq(
+      libraryDependencies ++= Seq(
         dependencies.couchbaseClient, dependencies.json
       )
     )
   )
 
+  lazy val dataCruncher = Project(
+    id = "data-cruncher",
+    base = file("data-cruncher"),
+    dependencies = Seq(dataStorage % "test->test;compile->compile"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies ++= dependencies.spark
+    )
+  )
+
   lazy val webApp = Project(
     id = "web-app",
-    base = file("web-app")
+    base = file("web-app"),
+    dependencies = Seq(dataStorage)
   ).enablePlugins(play.PlayScala)
-    .dependsOn(dataCruncher)
-    .settings(
-      libraryDependencies ++= Seq()
-    )
+
 
   // top level aggregate
   lazy val root = Project(
     id = "velocorner",
     base = file("."),
     settings = buildSettings,
-    aggregate = Seq(dataCruncher, webApp)
+    aggregate = Seq(dataStorage, dataCruncher, webApp)
   )
 }
