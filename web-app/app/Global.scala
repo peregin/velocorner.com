5e1bf90e5fc0ea85a1b8bc5b7d44345ae46e7d0b
levi <velocorner.com@gmail.com>
2015-03-14 19:44:54 +0100

    split configurations

--- web-app/app/Global.scala
+++ web-app/app/Global.scala
@@ -6,7 +6,16 @@ import velocorner.storage.Couchbase
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
-    Logger.info("connecting to storage...")
+    Logger.info("starting the application...")
+
+    // check included configuration file
+    val appSecret = app.configuration.getString("application.secret").mkString
+    Logger.info(s"application.secret: $appSecret")
+
+    // check main configuration file, must be provided separately with the token setups
+    val stravaAppToken = app.configuration.getString("strava.application.token").mkString
+    Logger.info(s"strava.application.token: $stravaAppToken")
+
     //val stats = Couchbase.logStats()
     //Logger.info(stats.mkString("\n"))
   }
