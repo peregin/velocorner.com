ac80b7b66ee1590eb7b48752e1b2d20ba5fb2301
levi <velocorner.com@gmail.com>
2016-02-01 22:38:22 +0100

    wait for flush

--- data-storage/src/test/scala/velocorner/manual/cleanup/FlushCouchbaseApp.scala
+++ data-storage/src/test/scala/velocorner/manual/cleanup/FlushCouchbaseApp.scala
@@ -1,5 +1,7 @@
 package velocorner.manual.cleanup
 
+import java.util.concurrent.TimeUnit
+
 import org.slf4s.Logging
 import velocorner.SecretConfig
 import velocorner.manual.MyMacConfig
@@ -7,13 +9,15 @@ import velocorner.storage.CouchbaseStorage
 import velocorner.util.Metrics
 
 
-object NukeAllFromCouchbaseApp extends App with Logging with Metrics with MyMacConfig {
+object FlushCouchbaseApp extends App with Logging with Metrics with MyMacConfig {
 
   log.info("connecting to couchbase bucket...")
   val storage = new CouchbaseStorage(SecretConfig.load().getBucketPassword)
   storage.initialize()
 
   val future = storage.client.flush()
+  val succeeded = future.get(10, TimeUnit.SECONDS)
+  log.info(s"flush succeeded $succeeded")
 
   storage.destroy()
 }
