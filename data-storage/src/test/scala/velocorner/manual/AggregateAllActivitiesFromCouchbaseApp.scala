08c5d3601609f03c052bb30a476cec2f166d69fc
levi <velocorner.com@gmail.com>
2016-09-18 22:11:15 +0200

    refactor

--- data-storage/src/test/scala/velocorner/manual/AggregateAllActivitiesFromCouchbaseApp.scala
+++ data-storage/src/test/scala/velocorner/manual/AggregateAllActivitiesFromCouchbaseApp.scala
@@ -1,20 +1,16 @@
 package velocorner.manual
 
 import org.slf4s.Logging
-import velocorner.SecretConfig
-import velocorner.storage.CouchbaseStorage
+import velocorner.storage.Storage
 import velocorner.util.Metrics
 
 
 object AggregateAllActivitiesFromCouchbaseApp extends App with Metrics with Logging with AggregateActivities with MyMacConfig {
 
-  val password = SecretConfig.load().getBucketPassword
-  log.info("connecting to the couchbase bucket...")
-
-  val storage = new CouchbaseStorage(password)
+  val storage = Storage.create("co")
   storage.initialize()
-  val progress = timed("aggregation")(storage.dailyProgressForAll(5))
 
+  val progress = timed("aggregation")(storage.dailyProgressForAll(5))
   progress foreach println
 
   log.info("done...")
