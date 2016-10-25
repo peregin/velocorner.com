2262a1fbc576c97434a240d4f9a0e2cdc276f8d6
levi <velocorner.com@gmail.com>
2016-10-25 08:15:00 +0200

    measures request from Withings

--- data-storage/src/main/scala/velocorner/feed/StravaActivityFeed.scala
+++ data-storage/src/main/scala/velocorner/feed/StravaActivityFeed.scala
@@ -1,18 +1,12 @@
 package velocorner.feed
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import org.asynchttpclient.proxy.ProxyServer
-import org.asynchttpclient.{DefaultAsyncHttpClientConfig, Realm}
 import org.slf4s.Logging
 import play.api.libs.ws.WSResponse
-import play.api.libs.ws.ahc.AhcWSClient
 import velocorner.SecretConfig
 import velocorner.model.{Activity, Athlete}
 import velocorner.util.JsonIo
 
 import scala.annotation.tailrec
-import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
@@ -41,33 +35,13 @@ object StravaActivityFeed {
   }
 }
 
-class StravaActivityFeed(maybeToken: Option[String], config: SecretConfig) extends ActivityFeed with Logging {
+class StravaActivityFeed(maybeToken: Option[String], val config: SecretConfig) extends HttpFeed with ActivityFeed with Logging {
 
   val token = maybeToken.getOrElse(config.getToken("strava")) // dedicated token after authentication or application generic
   val clientId = config.getId("strava")
   log.info(s"connecting to strava with token [$token] and clientId[$clientId]...")
-
   val authHeader = s"Bearer $token"
   val timeout = 10 seconds
-
-  val httpConfigBuilder = new DefaultAsyncHttpClientConfig.Builder()
-
-  // setup secure proxy if it is configured w/o authentication
-  for (proxyHost <- config.getProxyHost; proxyPort <- config.getProxyPort) {
-    val proxyServer = (config.getProxyUser, config.getProxyPassword) match {
-      case (Some(proxyUser), Some(proxyPassword)) =>
-        val realm = new Realm.Builder(proxyUser, proxyPassword).build()
-        new ProxyServer(proxyHost, proxyPort, 443, realm, List.empty.asJava)
-      case _ =>
-        new ProxyServer(proxyHost, proxyPort, 443, null, List.empty.asJava)
-    }
-    httpConfigBuilder.setProxyServer(proxyServer)
-  }
-
-  implicit val system = ActorSystem.create("ws")
-  implicit val materializer = ActorMaterializer()
-  val wsClient = new AhcWSClient(httpConfigBuilder.build())
-
 
   // clubs
   override def listRecentClubActivities(clubId: Long): List[Activity] = {
--- data-storage/src/main/scala/velocorner/feed/WithingsMeasureFeed.scala
+++ data-storage/src/main/scala/velocorner/feed/WithingsMeasureFeed.scala
@@ -3,17 +3,34 @@ package velocorner.feed
 import org.slf4s.Logging
 import velocorner.SecretConfig
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
 /**
 * Implementation to connect with Withings REST API
 */
 object WithingsMeasureFeed {
 
-  val baseUrl = "https://wbsapi.withings.net"
+  val baseUrl = "http://wbsapi.withings.net"
 }
 
-class WithingsMeasureFeed(maybeToken: Option[String], config: SecretConfig) extends MeasureFeed with Logging {
+class WithingsMeasureFeed(maybeToken: Option[String], val config: SecretConfig) extends HttpFeed with MeasureFeed with Logging {
 
   val token = maybeToken.getOrElse(config.getToken("withings")) // dedicated token after authentication or application generic
+  log.info(s"connecting to strava with token [$token]...")
 
-  override def listMeasures: String = ???
+  val authHeader = s"Bearer $token"
+  val timeout = 10 seconds
+
+  // TODO: setup
+  // oauth_consumer_key =
+  // oauth_nonce = AAA
+  // oauth_timestamp = 1477376000
+  // oauth_token = AAA
+  // oauth_signature = BBB
+  override def listMeasures: String = {
+    val response = wsClient.url(s"${WithingsMeasureFeed.baseUrl}/measure?action=getmeas").withHeaders(("Authorization", authHeader)).get()
+    Await.result(response, timeout).body
+  }
 }
