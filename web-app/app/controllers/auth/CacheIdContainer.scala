d641dfa34c87b5ada98abea5db2091a1e9799756
levi <velocorner.com@gmail.com>
2017-12-28 21:41:09 +0100

    postfix operator

--- web-app/app/controllers/auth/CacheIdContainer.scala
+++ web-app/app/controllers/auth/CacheIdContainer.scala
@@ -10,6 +10,7 @@ import scala.annotation.tailrec
 import scala.concurrent.duration._
 import scala.reflect.ClassTag
 import scala.util.Random
+import scala.language.postfixOps
 
 class CacheIdContainer[Id: ClassTag] @Inject()(cache: SyncCacheApi) {
 
@@ -27,7 +28,7 @@ class CacheIdContainer[Id: ClassTag] @Inject()(cache: SyncCacheApi) {
   @tailrec
   private[auth] final def generate: AuthenticityToken = {
     val table = "abcdefghijklmnopqrstuvwxyz1234567890_.~*'()"
-    val token = Iterator.continually(random.nextInt(table.size)).map(table).take(64).mkString
+    val token = Iterator.continually(random.nextInt(table.length)).map(table).take(64).mkString
     if (get(token).isDefined) generate else token
   }
 
@@ -48,7 +49,7 @@ class CacheIdContainer[Id: ClassTag] @Inject()(cache: SyncCacheApi) {
     cache.remove(userId.toString + userIdSuffix)
   }
 
-  def get(token: AuthenticityToken) = cache.get[Id](token + tokenSuffix)
+  def get(token: AuthenticityToken): Option[Id] = cache.get[Id](token + tokenSuffix)
 
   private[auth] def store(token: AuthenticityToken, userId: Id, timeoutInSeconds: Int) {
     cache.set(token + tokenSuffix, userId, timeoutInSeconds seconds)
