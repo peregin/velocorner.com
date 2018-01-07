7945ec511861d37dd788a9a793d0f3b7e0b29976
levi <velocorner.com@gmail.com>
2018-01-07 22:28:59 +0100

    connect with oauth1

--- data-provider/src/main/scala/velocorner/feed/WithingsMeasureFeed.scala
+++ data-provider/src/main/scala/velocorner/feed/WithingsMeasureFeed.scala
@@ -2,18 +2,21 @@ package velocorner.feed
 
 import org.slf4s.Logging
 import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
+import play.shaded.ahc.org.asynchttpclient.{Request, RequestBuilderBase}
 import velocorner.SecretConfig
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import collection.JavaConversions._
 
 /**
 * Implementation to connect with Withings REST API
 */
 object WithingsMeasureFeed {
 
-  val baseUrl = "https://api.health.nokia.com/v2"
+  // for getting body measures
+  val baseUrl = "https://api.health.nokia.com"
 
   def consumerKey(config: SecretConfig) = ConsumerKey(config.getToken("withings"), config.getSecret("withings"))
 }
@@ -24,14 +27,36 @@ class WithingsMeasureFeed(userId: Long, token: RequestToken, val config: SecretC
 
   lazy val timeout = 10 seconds
 
-  private def signer = OAuthCalculator(WithingsMeasureFeed.consumerKey(config), token)
+  private def signer = new OAuthCalculator(WithingsMeasureFeed.consumerKey(config), token) {
+    override def calculateAndAddSignature(request: Request, requestBuilder: RequestBuilderBase[_]): Unit = {
+      super.calculateAndAddSignature(request, requestBuilder)
+      val maybeAuthHeader = request.getHeaders.toSeq.filter(m => m.getKey.compareToIgnoreCase("Authorization") == 0).headOption
+      maybeAuthHeader.foreach{h =>
+        val auth = h.getValue
+        log.info(s"auth header: $auth")
+        val pairs = auth.stripPrefix("OAuth").split(',').map(_.trim)
+        pairs.foreach{p =>
+          val keyValue = p.split('=')
+          if (keyValue.length > 1) {
+            val paramName = keyValue(0).trim
+            val paramValue = keyValue(1).trim.stripPrefix("\"").stripSuffix("\"")
+            log.info(s"$paramName=$paramValue")
+            requestBuilder.addQueryParam(paramName, paramValue)
+          }
+        }
+      }
+    }
+  }
 
   override def listMeasures: String = {
-    val response = ws(_
-      .url(s"${WithingsMeasureFeed.baseUrl}/measure")
-      .withQueryStringParameters(("action", "getmeas"), ("userid", userId.toString))
+    val response = ws{_.url(s"${WithingsMeasureFeed.baseUrl}/measure")
+      .withQueryStringParameters(
+        ("action", "getmeas"),
+        ("userid", userId.toString)
+      )
       .sign(signer)
-      .get())
+      .get()
+    }
     Await.result(response, timeout).body
   }
 }
--- project/build.scala
+++ project/build.scala
@@ -14,7 +14,7 @@ object dependencies {
 
   val sparkVersion = "2.2.1"
   val logbackVersion = "1.2.3"
-  val elasticVersion = "6.1.1"
+  val elasticVersion = "6.1.2"
   val specsVersion = "3.7"
   val orientDbVersion = "2.2.31"
   val log4jVersion = "2.10.0"
