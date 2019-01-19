0b1f43443f0bc8d1dc95402e209e0c9b1519c828
levi <velocorner.com@gmail.com>
2019-01-19 10:30:51 +0100

    test model

--- data-provider/src/test/scala/velocorner/model/weather/WeatherResponseSpec.scala
+++ data-provider/src/test/scala/velocorner/model/weather/WeatherResponseSpec.scala
@@ -18,6 +18,13 @@ class WeatherResponseSpec extends Specification {
 
       val first = forecast.list.head
       first.dt.compareTo(DateTime.parse("2019-01-19T01:00:00.000+01:00")) === 0
+      first.main.temp === -3.71f
+      first.main.humidity === 89
+      first.weather must haveSize(1)
+
+      val info = first.weather.head
+      info.main === "Clear"
+      info.icon === "01n"
     }
   }
 }
