4409c3cedd0fa8ad8c30bd5f1e451b87b6bd8080
levi <velocorner.com@gmail.com>
2019-01-28 19:50:41 +0100

    calc min and max for a day

--- data-provider/src/main/scala/velocorner/model/DailyWeather.scala
+++ data-provider/src/main/scala/velocorner/model/DailyWeather.scala
@@ -12,9 +12,15 @@ object DailyWeather {
   def list(entries: Iterable[WeatherForecast]): Iterable[DailyWeather] = {
     entries
       .groupBy(wf => new DateTime(wf.timestamp).toLocalDate)
-      .map{ case (day, wfList) => DailyWeather(day, wfList.map(_.forecast)) }
+      .map{ case (day, wfList) => from(day, wfList.map(_.forecast)) }
       .toSeq.sortBy(_.day.toString)
+  }
+
+  def from(day: LocalDate, points: Iterable[Weather]): DailyWeather = {
+    val min = points.map(_.main.temp_min).min
+    val max = points.map(_.main.temp_max).max
+    new DailyWeather(day, points, min, max)
   }
 }
 
-case class DailyWeather(day: LocalDate, points: Iterable[Weather])
+case class DailyWeather(day: LocalDate, points: Iterable[Weather], temp_min: Float, temp_max: Float)
