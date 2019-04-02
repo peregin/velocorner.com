9b5241b796f68ff8f8d347e6792a212af57b7482
levi <velocorner.com@gmail.com>
2019-04-02 21:32:13 +0200

    show env mode

--- web-app/app/controllers/ApplicationController.scala
+++ web-app/app/controllers/ApplicationController.scala
@@ -1,21 +1,22 @@
 package controllers
 
-import javax.inject.Inject
 import controllers.auth.AuthChecker
+import javax.inject.Inject
 import play.Logger
+import play.api.{Environment, Mode}
 import play.api.cache.SyncCacheApi
 import play.api.mvc._
-
+import scalaz.Scalaz._
 import scalaz._
-import Scalaz._
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class ApplicationController @Inject()
-(components: ControllerComponents, val cache: SyncCacheApi,
- val connectivity: ConnectivitySettings, strategy: RefreshStrategy)
-(implicit assets: AssetsFinder) extends AbstractController(components) with AuthChecker {
+  (environment: Environment, components: ControllerComponents,
+   val cache: SyncCacheApi, val connectivity: ConnectivitySettings,
+   strategy: RefreshStrategy)
+  (implicit assets: AssetsFinder) extends AbstractController(components) with AuthChecker {
 
   private val logger = Logger.of(this.getClass)
 
@@ -44,7 +45,7 @@ class ApplicationController @Inject()
 
   private def getPageContext(title: String)(implicit request: Request[AnyContent]) = {
     val maybeAccount = loggedIn
-    val context = PageContext(title, maybeAccount,
+    val context = PageContext(title, maybeAccount, environment.mode,
       connectivity.secretConfig.isWithingsEnabled(),
       connectivity.secretConfig.isWeatherEnabled(), WeatherCookie.retrieve
     )
