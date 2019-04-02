c01a0561a14d56870494351df7b903dc96163006
levi <velocorner.com@gmail.com>
2019-04-02 22:28:16 +0200

    update tests

--- web-app/app/controllers/ErrorController.scala
+++ web-app/app/controllers/ErrorController.scala
@@ -1,16 +1,17 @@
 package controllers
 
 import javax.inject.Inject
+import play.api.Environment
 import play.api.http.DefaultHttpErrorHandler
 import play.api.mvc.{RequestHeader, Result}
 import play.api.mvc.Results._
 
 import scala.concurrent.Future
 
-class ErrorController @Inject()(implicit assets: AssetsFinder) extends DefaultHttpErrorHandler {
+class ErrorController @Inject()(environment: Environment)(implicit assets: AssetsFinder) extends DefaultHttpErrorHandler {
 
   override protected def onNotFound(request: RequestHeader, message: String): Future[Result] = {
-    val context = PageContext(title = "Not Found", account = None,
+    val context = PageContext(title = "Not Found", account = None, environment.mode,
       isWithingsEnabled = false,
       isWeatherEnabled = false, weatherLocation = "")
     Future.successful(NotFound(views.html.notFound(context)(assets = assets)))
