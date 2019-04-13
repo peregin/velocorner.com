dea41082b31fcb623d6aa8d2aad7f7cd757e32dc
levi <velocorner.com@gmail.com>
2019-04-13 20:38:49 +0200

    set weather cookie max age to one week

--- web-app/app/controllers/WeatherCookie.scala
+++ web-app/app/controllers/WeatherCookie.scala
@@ -2,14 +2,18 @@ package controllers
 
 import org.apache.commons.codec.binary.Base64
 import play.api.mvc.{AnyContent, Cookie, Request}
+import concurrent.duration._
+import scalaz.syntax.std.option._
 
 object WeatherCookie {
 
+  // keep it for a week
+  val cookieAgeDuration = 7 days
   val name = "weather_location"
 
   def create(location: String): Cookie = {
     val encoded = Base64.encodeBase64String(location.getBytes)
-    Cookie(name, encoded)
+    Cookie(name, encoded, maxAge = cookieAgeDuration.toSeconds.toInt.some)
   }
 
   def retrieve(implicit request: Request[AnyContent]): String = {
