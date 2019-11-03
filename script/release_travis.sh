e619f1c4d52d63ece2e0aee12a547295c2c6791a
levi <velocorner.com@gmail.com>
2019-11-03 16:38:20 +0100

    ci8

--- script/release_travis.sh
+++ script/release_travis.sh
@@ -6,5 +6,5 @@ fi
 git config --global user.name "Deploy CI"
 git remote rm origin
 git remote add origin https://peregin:${GH_TOKEN}@github.com/peregin/velocorner.com.git > /dev/null 2>&1
-git checkout "$TRAVIS_BRANCH"
+git checkout "$TRAVIS_BRANCH" --set-upstream origin
 sbt "release skip-tests with-defaults"
