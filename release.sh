2d017e2f03afb9f0fb425aacd8618766366505d9
levi <velocorner.com@gmail.com>
2019-11-03 15:20:37 +0100

    ci2

--- release.sh
+++ release.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+if [ -z "$TRAVIS_BRANCH" ]; then
+  echo "TRAVIS_BRANCH environment variable must be set"
+  exit 1
+fi
+git checkout "$TRAVIS_BRANCH"
 sbt "release with-defaults"
