6af497c458e51e19df3b4ea7709b0a1edd5d069f
levi <velocorner.com@gmail.com>
2019-11-03 16:19:12 +0100

    ci6

--- script/release_travis.sh
+++ script/release_travis.sh
@@ -3,5 +3,6 @@ if [ -z "$TRAVIS_BRANCH" ]; then
   echo "TRAVIS_BRANCH environment variable must be set"
   exit 1
 fi
+git config --global user.name "Deploy CI"
 git checkout "$TRAVIS_BRANCH"
 sbt "release skip-tests with-defaults"
