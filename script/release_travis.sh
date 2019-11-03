579faf7da46d9380adc4ddcd8a71d8eefb38d594
levi <velocorner.com@gmail.com>
2019-11-03 16:46:20 +0100

    ci10

--- script/release_travis.sh
+++ script/release_travis.sh
@@ -3,8 +3,9 @@ if [ -z "$TRAVIS_BRANCH" ]; then
   echo "TRAVIS_BRANCH environment variable must be set"
   exit 1
 fi
-git config --global user.name "Deploy CI"
 #git remote rm origin
-git remote add origin https://peregin:${GH_TOKEN}@github.com/peregin/velocorner.com.git > /dev/null 2>&1
-git checkout "$TRAVIS_BRANCH"
+#git remote add origin https://peregin:${GH_TOKEN}@github.com/peregin/velocorner.com.git > /dev/null 2>&1
+#git checkout "$TRAVIS_BRANCH"
+git clone https://peregin:${GH_TOKEN}@github.com/peregin/velocorner.com.git > /dev/null 2>&1
+git config --global user.name "Deploy CI"
 sbt "release skip-tests with-defaults"
