63aab7b19c3d4cbd5426e2551ad27413ddb444a5
levi <velocorner.com@gmail.com>
2019-11-09 21:09:49 +0100

    add router to react [skip ci]

--- web-front/src/App.js
+++ web-front/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Client from "./Client";
+import About from "./About";
+
+import Header from './components/Header';
+
+import reactLogo from './images/react.svg';
+import playLogo from './images/play.svg';
+import scalaLogo from './images/scala.png';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Tech = ({ match }) => {
+  return <div>Current Route: {match.params.tech}</div>
+};
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {title: 'Velocorner'};
+  }
+
+  async componentDidMount() {
+    Client.getSummary(summary => {
+      this.setState({
+        title: summary.content
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        <Header/>
+
+          <h1>Welcome to {this.state.title}!</h1>
+          <Route path="/about" component={About} />
+          <Route path="/:tech" component={Tech} />
+          <nav>
+              <Link to="scala" >
+                <img  width="450" height="300"  src={scalaLogo} alt="Scala Logo" />
+              </Link>
+              <Link to="play" >
+                <img width="400" height="400" src={playLogo} alt="Play Framework Logo" />
+              </Link>
+              <Link to="react" >
+                <img width="400" height="400" src={reactLogo} className="App-logo" alt="React Logo"/>
+              </Link>
+          </nav>
+          <div>
+            <h2>Check out the project on GitHub</h2>
+            <h3>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/peregin/velocorner.com">
+                velocorner
+              </a>
+            </h3>
+          </div>
+
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default App;;
---
+++ web-front/src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {Link, BrowserRouter} from 'react-router-dom';
+
+class Header extends Component {
+    render() {
+        return (
+            <header role="banner">
+                <section className="header-wrapper">
+                    <Link id="logo" to="/" title="Velocorner" />
+                    <nav>
+                        <ul>
+                            <li><Link to="/" className={ this.props.page == "main" ? 'current' : '' }>Home</Link></li>
+                            <li><Link to="/about" className={ this.props.page == "about" ? 'current' : '' }>About</Link></li>
+                        </ul>
+                    </nav>
+                </section>
+            </header>
+        )
+    }
+}
+
+export default Header;
