38fa32fa26140e7abf12ba0d1c68bf2f49d55095
levi <velocorner.com@gmail.com>
2019-12-02 22:11:45 +0100

    clean up data [skip ci]

--- data-analytics/analyze.py
+++ data-analytics/analyze.py
@@ -3,12 +3,23 @@ import pandas as pd
 
 def analyze():
     name = "data/432909.json.gz"
-    print(f"analyzing {name} ...")
-    df = pd\
-        .read_json(name, orient="values", compression="gzip")\
-        .drop(["type", "external_id", "upload_id", "athlete", "name"], axis=1)
+    print(f"loading and cleaning data from {name} ...")
+    df = pd \
+        .read_json(name, orient="values", compression="gzip") \
+        .drop(
+        ["type", "external_id", "upload_id", "athlete", "name", "resource_state", "average_heartrate", "max_heartrate",
+         "start_date_local", "gear_id"],
+        axis=1)
+    print("convert units ...")
+    df['distance'] = df['distance'] / 1000
+    df['distance'] = df['distance'].round(2)
+    df['average_speed'] = df['average_speed'] * 18 / 5
+    df['average_speed'] = df['average_speed'].round(2)
+
     print(df.info())
     print(df.head())
+
+    print("done ...")
 
 
 if __name__ == "__main__":
