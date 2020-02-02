8eda3e212a1665b36abec40be26c412217479b82
levi <velocorner.com@gmail.com>
2020-02-02 10:06:52 +0100

    notes on API gateway [skip ci]

--- gateway-service/src/main/scala/velocorner/gateway/GatewayService.scala
+++ gateway-service/src/main/scala/velocorner/gateway/GatewayService.scala
@@ -4,6 +4,19 @@ import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.http.{Controller, HttpServer}
 
+/**
+  * API Gateway
+  * - single entry point to the stack
+  * - expose API from schema, see app.apibuilder.io
+  * - convert and proxy to internal requests (e.g. Thrift or TypeScript)
+  * - authentication (e.g. JWT)
+  * - mapping to the internal service topology, use service discovery for calling services
+  * In addition:
+  * - caching
+  * - retry policies, circuit breaking
+  * - throttling
+  * - IP white/black-listing
+  */
 object GatewayService extends HttpServer {
 
   class InfoController extends Controller {
