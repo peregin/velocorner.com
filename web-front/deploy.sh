3be76a7a545b3713040dc1c7c626ddc53781786c
levi <velocorner.com@gmail.com>
2020-02-03 22:25:41 +0100

    update react deployment script

--- web-front/deploy.sh
+++ web-front/deploy.sh
@@ -2,6 +2,20 @@
 
 set -e
 
-docker run --rm -p 3000:3000 peregin/web-front:latest
+npm version patch
+CURRENT_VERSION=$(npm run version --silent)
+echo "Current version is $CURRENT_VERSION"
+#git commit -a -m 'Setting version to $CURRENT_VERSION'
+npm run clean
+npm install
+npm run build
+
+docker build . -t peregin/web-front:latest
+# test the image
+# docker run --rm -p 3000:3000 peregin/web-front:latest
 docker push peregin/web-front:latest
+
+#git push
+
+echo "Successfully deployed..."
 
