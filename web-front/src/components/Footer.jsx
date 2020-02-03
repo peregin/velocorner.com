5650851fdfeb46e615508d84d031f298d71a41fa
Levente Fall <levente.fall@tundra.com>
2020-02-03 21:06:11 +0100

    style react footer [skip ci]

--- web-front/src/components/Footer.jsx
+++ web-front/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
 
 class Footer extends Component {
     render() {
@@ -7,32 +6,40 @@ class Footer extends Component {
             <footer role="banner">
       
                 <section>
-                    <div id="footer-v2" class="footer-v2">
-                        <div class="footer">
-                            <div class="container">
-                                <div class="row">
+                    <div id="footer-v2" className="footer-v2">
+                        <div className="footer">
+                            <div className="container">
+                                <div className="row">
                                     
-                                    <div class="col-md-8 md-margin-bottom-40">
-                                        <script src="http://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe title="Instagram" src="//lightwidget.com/widgets/aa5ba80ed03c5bc187d1419e2a57bac8.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+                                    <div className="col-md-8 md-margin-bottom-40">
+                                        <script src="http://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+                                        <iframe title="Instagram" src="//lightwidget.com/widgets/aa5ba80ed03c5bc187d1419e2a57bac8.html" 
+                                          scrolling="no" allowtransparency="true" className="lightwidget-widget" 
+                                          style = {{
+                                              width: '100%',
+                                              border: '0',
+                                              overflow: 'hidden'
+                                          }}>
+                                        </iframe>
                                     </div>
-                                    <div class="col-md-4 md-margin-bottom-40">
-                                        <div class="headline"><h2 class="heading-sm">Contact Us</h2></div>
-                                        <ul class="social-icons">
-                                            <li class="VC_social_icon"><a href="https://www.instagram.com/peregin" data-original-title="Instagram" class="rounded-x social_instagram">Instagram</a><small>INSTAGRAM</small></li>
-                                            <li class="VC_social_icon"><a href="https://github.com/peregin/velocorner.com" data-original-title="Github" class="rounded-x social_github">GitHub</a><small>GITHUB</small></li>
-                                            <li class="VC_social_icon"><a href="https://www.strava.com/clubs/velocorner" data-original-title="Strava" class="rounded-x social_strava">Strava</a><small>STRAVA</small></li>
-                                            <li class="VC_social_icon"><a href="https://www.youtube.com/channel/UCekBQAfLHviXTvB1kVjzBZQ" data-original-title="Youtube" class="rounded-x social_youtube">Youtube</a><small>YOUTUBE</small></li>
-                                            <li class="VC_social_icon"><a href="https://twitter.com/velocorner_com" data-original-title="Twitter" class="rounded-x social_twitter">Twitter</a><small>TWITTER</small></li>
-                                            <li class="VC_social_icon"><a href="mailto:velocorner.com@@gmail.com" data-original-title="Email" class="rounded-x social_email">velocorner.com@@gmail.com</a><small>EMAIL</small></li>
+                                    <div className="col-md-4 md-margin-bottom-40">
+                                        <div className="headline"><h2 className="heading-sm">Contact Us</h2></div>
+                                        <ul className="social-icons">
+                                            <li className="VC_social_icon"><a href="https://www.instagram.com/peregin" data-original-title="Instagram" className="rounded-x social_instagram">Instagram</a><small>INSTAGRAM</small></li>
+                                            <li className="VC_social_icon"><a href="https://github.com/peregin/velocorner.com" data-original-title="Github" className="rounded-x social_github">GitHub</a><small>GITHUB</small></li>
+                                            <li className="VC_social_icon"><a href="https://www.strava.com/clubs/velocorner" data-original-title="Strava" className="rounded-x social_strava">Strava</a><small>STRAVA</small></li>
+                                            <li className="VC_social_icon"><a href="https://www.youtube.com/channel/UCekBQAfLHviXTvB1kVjzBZQ" data-original-title="Youtube" className="rounded-x social_youtube">Youtube</a><small>YOUTUBE</small></li>
+                                            <li className="VC_social_icon"><a href="https://twitter.com/velocorner_com" data-original-title="Twitter" className="rounded-x social_twitter">Twitter</a><small>TWITTER</small></li>
+                                            <li className="VC_social_icon"><a href="mailto:velocorner.com@@gmail.com" data-original-title="Email" className="rounded-x social_email">velocorner.com@@gmail.com</a><small>EMAIL</small></li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="copyright">
-                            <div class="container">
-                                <p class="text-center">2015 - TODO &copy; All Rights Reserved <a href="http://velocorner.com">Velocorner</a></p>
+                        <div className="copyright">
+                            <div className="container">
+                                <p className="text-center">2015 - {(new Date().getFullYear())} &copy; All Rights Reserved <a target="_blank" href="http://velocorner.com">Velocorner</a></p>
                             </div>
                         </div>
 
