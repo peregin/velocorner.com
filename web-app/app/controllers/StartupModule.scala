c81532054b0c5ac975bf5e565179fc3e0c596656
levi <velocorner.com@gmail.com>
2020-05-02 10:53:19 +0200

    add up time to the status info

--- web-app/app/controllers/StartupModule.scala
+++ web-app/app/controllers/StartupModule.scala
@@ -1,10 +1,46 @@
 package controllers
 
 import com.google.inject.AbstractModule
+import com.typesafe.scalalogging.LazyLogging
+import javax.inject.Singleton
+import org.joda.time.Period
+import org.joda.time.format.PeriodFormatterBuilder
 
 class StartupModule extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[StartupService]).asEagerSingleton
   }
+}
+
+object StartupService {
+
+  val startupTime = System.currentTimeMillis()
+
+  val formatter = new PeriodFormatterBuilder()
+    .appendDays()
+    .appendSuffix(" day,", " days,")
+    .appendSeparator(" ")
+    .printZeroIfSupported()
+    .minimumPrintedDigits(2)
+    .appendHours()
+    .appendSeparator(":")
+    .appendMinutes()
+    .printZeroIfSupported()
+    .minimumPrintedDigits(2)
+    .appendSeparator(":")
+    .appendSeconds()
+    .minimumPrintedDigits(2)
+    .toFormatter()
+
+  def elapsedTimeText(): String = elapsedTimeText(System.currentTimeMillis() - startupTime)
+
+  def elapsedTimeText(duration: Long): String = formatter.print(new Period(duration).normalizedStandard())
+
+}
+
+@Singleton
+class StartupService extends LazyLogging {
+
+  logger.info(s"startup service initializing at ${StartupService.startupTime} ...")
 }
