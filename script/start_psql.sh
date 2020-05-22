fda0fdac2c0c82e028ac74b2c77b48002df63f2b
Levente Fall <levente.fall@gmail.com>
2020-05-22 21:56:31 +0200

    add script to start psql [skip ci]

---
+++ script/start_psql.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CONTAINER_REPO="velo_repo"
+if [[ $(docker inspect -f '{{.State.Running}}' $CONTAINER_REPO) = "true" ]]; then
+  echo "$CONTAINER_REPO is already running ..."
+else
+  docker run -d -p 5432:5432 \
+    --rm --name $CONTAINER_REPO \
+    -e POSTGRES_DB=velocorner \
+    -e POSTGRES_USER=velocorner \
+    -e POSTGRES_PASSWORD=velocorner \
+    postgres:12.2-alpine
+  echo "$CONTAINER_REPO has been started ..."
+fi
