f4ebf5884759cd87bb5836c2969359ebaaa36b6d
levi <velocorner.com@gmail.com>
2020-10-17 13:17:28 +0200

    bump libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,14 +9,14 @@ object Dependencies {
   val logbackVersion = "1.2.3"
   val doobieVersion = "0.9.2"
   val orientDbVersion = "3.1.3"
-  val mongoDbVersion = "4.1.0"
+  val mongoDbVersion = "4.1.1"
   val rethinkDbVersion = "2.4.0"
-  val flywayVersion = "7.0.2"
+  val flywayVersion = "7.0.4"
   val elasticVersion = "7.9.1"
   val finatraVersion = "20.9.0"
   val playWsVersion = "2.1.2" // standalone version
   val playJsonVersion = "2.9.1"
-  val scalaSpecVersion = "4.10.4"
+  val scalaSpecVersion = "4.10.5"
   val scalaTestVersion = "3.0.5"
   val mockitoVersion = "3.5.13"
 }
