e6d5fd8b38a317797016504c4d3544aa99e435c6
Levente Fall <levente.fall@tundra.com>
2021-01-29 21:23:05 +0100

    wip [skip ci]

--- web-app/app/controllers/DemoController.scala
+++ web-app/app/controllers/DemoController.scala
@@ -2,19 +2,23 @@ package controllers
 
 import controllers.util.WebMetrics
 import play.api.mvc.{AbstractController, ControllerComponents}
+import velocorner.util.DemoActivityUtils
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-
-class DemoController @Inject()(val connectivity: ConnectivitySettings, components: ControllerComponents)
-  extends AbstractController(components) with WebMetrics {
-
+class DemoController @Inject() (val connectivity: ConnectivitySettings, components: ControllerComponents)
+    extends AbstractController(components)
+    with WebMetrics {
 
   // demo ytd data
   // route mapped to /api/demo/statistics/ytd/:action
-  def ytdStatistics(action: String)= Action.async { timedRequest(s"demo ytd data for $action") { implicit request =>
-    Future(Ok)
-  }}
+  def ytdStatistics(action: String) = Action.async {
+    timedRequest(s"demo ytd data for $action") { implicit request =>
+      val activities = DemoActivityUtils.generate()
+
+      Future(Ok)
+    }
+  }
 }
