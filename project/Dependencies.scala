770298083f58e5b77fffd1ff99f0645324a502b0
levi <velocorner.com@gmail.com>
2021-04-22 22:07:30 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,14 +4,14 @@ object Dependencies {
   val projectScalaVersion = "2.13.5"
   val scala12Version = "2.12.12" // for some modules using Spark (explicitly 2.12.12)
 
-  val catsVersion = "2.5.0"
+  val catsVersion = "2.6.0"
   val mouseVersion = "1.0.2"
   val zioVersion = "1.0.6"
   val zioLoggingVersion = "0.5.8"
   val shapelessVersion = "2.3.4"
   val logbackVersion = "1.2.3"
-  val doobieVersion = "0.12.1"
-  val orientDbVersion = "3.1.10"
+  val doobieVersion = "0.13.0"
+  val orientDbVersion = "3.1.11"
   val mongoDbVersion = "4.2.3"
   val rethinkDbVersion = "2.4.4"
   val flywayVersion = "7.8.1"
@@ -21,8 +21,8 @@ object Dependencies {
   val playJsonVersion = "2.9.2"
   val squantsVersion = "1.7.4"
   val sparkVersion = "3.1.1"
-  val scalaTestVersion = "3.2.7"
-  val mockitoVersion = "3.8.0"
+  val scalaTestVersion = "3.2.8"
+  val mockitoVersion = "3.9.0"
 
   val springVersion = "2.4.5" // java world
 }
