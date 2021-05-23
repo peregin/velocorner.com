1f021a70289e307fba409faa18d19a8902fe3c57
levi <velocorner.com@gmail.com>
2021-05-23 14:25:06 +0200

    update libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,14 +7,14 @@ object Dependencies {
   val catsVersion = "2.6.1"
   val mouseVersion = "1.0.2"
   val zioVersion = "1.0.8"
-  val zioLoggingVersion = "0.5.8"
-  val shapelessVersion = "2.3.6"
+  val zioLoggingVersion = "0.5.9"
+  val shapelessVersion = "2.3.7"
   val logbackVersion = "1.2.3"
-  val doobieVersion = "0.13.2"
+  val doobieVersion = "0.13.3"
   val orientDbVersion = "3.1.11"
   val mongoDbVersion = "4.2.3"
   val rethinkDbVersion = "2.4.4"
-  val flywayVersion = "7.9.0"
+  val flywayVersion = "7.9.1"
   val elasticVersion = "7.12.0"
   val finatraVersion = "21.2.0"
   val playWsVersion = "2.1.3" // standalone version
@@ -24,5 +24,5 @@ object Dependencies {
   val scalaTestVersion = "3.2.9"
   val mockitoVersion = "3.10.0"
 
-  val springVersion = "2.4.5" // java world
+  val springVersion = "2.5.0" // java world
 }
