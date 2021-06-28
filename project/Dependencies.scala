47e94160bf8821ac5d33674b5dc9dfc1db1dc8f7
levi <velocorner.com@gmail.com>
2021-06-28 09:25:29 +0200

    bump scala spark libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Dependencies {
 
   // update travis setup as well with the same scala version
   val projectScalaVersion = "2.13.6"
-  val scala12Version = "2.12.12" // for some modules using Spark (explicitly 2.12.12)
+  val scala12Version = "2.12.14" // for some modules using Spark (explicitly 2.12.12)
 
   val catsVersion = "2.6.1"
   val mouseVersion = "1.0.3"
