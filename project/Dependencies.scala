b12c80a9898684a2b754dd4caa573e2198a8c0f9
levi <velocorner.com@gmail.com>
2021-11-20 09:08:59 +0100

    bump libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val catsVersion = "2.6.1"
   val mouseVersion = "1.0.7"
   val zioVersion = "1.0.12"
-  val zioLoggingVersion = "0.5.13"
+  val zioLoggingVersion = "0.5.14"
   val shapelessVersion = "2.3.7"
   val argonautVersion = "6.3.7"
   val logbackVersion = "1.2.7"
@@ -14,7 +14,7 @@ object Dependencies {
   val orientDbVersion = "3.1.15"
   val mongoDbVersion = "4.4.0"
   val rethinkDbVersion = "2.4.4"
-  val flywayVersion = "8.0.4"
+  val flywayVersion = "8.0.5"
   val elasticVersion = "7.12.3"
   val finatraVersion = "21.2.0"
   val playWsVersion = "2.1.3" // standalone version
@@ -22,7 +22,7 @@ object Dependencies {
   val squantsVersion = "1.8.3"
   val sparkVersion = "3.2.0"
   val scalaTestVersion = "3.2.10"
-  val mockitoVersion = "4.0.0"
+  val mockitoVersion = "4.1.0"
 
-  val springVersion = "2.5.6" // java world
+  val springVersion = "2.6.0" // java world
 }
