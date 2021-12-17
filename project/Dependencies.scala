60cf5604e2bdcd1f90cb70e97b525d7edb73066b
levi <velocorner.com@gmail.com>
2021-12-17 19:50:10 +0100

    bump sbt to 1.5.7 and log version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,25 +3,25 @@ object Dependencies {
   val projectScalaVersion = "2.13.7"
 
   val catsVersion = "2.7.0"
-  val mouseVersion = "1.0.7"
-  val zioVersion = "1.0.12"
+  val mouseVersion = "1.0.8"
+  val zioVersion = "1.0.13"
   val zioLoggingVersion = "0.5.14"
   val shapelessVersion = "2.3.7"
   val argonautVersion = "6.3.7"
-  val logbackVersion = "1.2.7"
+  val logbackVersion = "1.2.9"
   val doobieVersion = "0.13.4"
   val orientDbVersion = "3.2.3"
   val mongoDbVersion = "4.4.0"
   val rethinkDbVersion = "2.4.4"
-  val flywayVersion = "8.2.1"
-  val elasticVersion = "7.15.6"
+  val flywayVersion = "8.2.2"
+  val elasticVersion = "7.16.0"
   val finatraVersion = "21.2.0"
   val playWsVersion = "2.1.6" // standalone version
   val playJsonVersion = "2.9.2"
   val squantsVersion = "1.8.3"
   val sparkVersion = "3.2.0"
   val scalaTestVersion = "3.2.10"
-  val mockitoVersion = "4.1.0"
+  val mockitoVersion = "4.2.0"
 
   val springVersion = "2.6.1" // java world
 }
