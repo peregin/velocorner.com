df394acfbae1a07ce45c34ab8eaaccde1762b820
Levente Fall <levente.fall@tundra.com>
2021-12-21 21:37:32 +0100

    add scala steward - update dir [skip ci]

--- script/scala_steward.sh
+++ script/scala_steward.sh
@@ -2,11 +2,9 @@
 
 docker run -v /Users/tundra/Downloads/private/dev/velocorner.com/script/scala-steward:/opt/scala-steward -it fthomas/scala-steward:latest \
   --workspace  "/opt/scala-steward/workspace" \
-  --repos-file "/opt/scala/repos.md" \
+  --repos-file "/opt/scala-steward/repos.md" \
   --git-author-email velocorner.com@gmail.com \
   --vcs-api-host "https://api.github.com" \
   --vcs-login scala_steward \
   --git-ask-pass "/opt/scala-steward/token.sh" \
-  --sign-commits \
   --do-not-fork
-
