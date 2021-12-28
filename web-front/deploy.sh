85891cf3f161970468b10d34ad9a072917578fa5
levi <velocorner.com@gmail.com>
2021-12-28 21:28:25 +0100

    disable FE tagging

--- web-front/deploy.sh
+++ web-front/deploy.sh
@@ -13,9 +13,10 @@ docker build . -t peregin/web-front:latest
 # docker run --rm -p 3000:3000 peregin/web-front:latest
 docker push peregin/web-front:latest
 
-git commit -a -m "Setting web-front version to $CURRENT_VERSION [skip ci]"
-TAG_TEXT="web-front-v$CURRENT_VERSION"
-git tag -a $TAG_TEXT -m "$TAG_TEXT"
+# ignore tagging the build, currently is tagged when building the BE
+#git commit -a -m "Setting web-front version to $CURRENT_VERSION [skip ci]"
+#TAG_TEXT="web-front-v$CURRENT_VERSION"
+#git tag -a $TAG_TEXT -m "$TAG_TEXT"
 
 echo "Successfully deployed..."
 
