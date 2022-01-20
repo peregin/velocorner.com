c1a39405ca5232109532b0e3ed4e1157e7907f50
Levente Fall <levente.fall@tundra.com>
2022-01-20 20:30:32 +0100

    adapt start script [skip ci]

--- script/start_psql.sh
+++ script/start_psql.sh
@@ -4,6 +4,7 @@ CONTAINER_REPO="velo_repo"
 if [[ $(docker inspect -f '{{.State.Running}}' $CONTAINER_REPO) = "true" ]]; then
   echo "$CONTAINER_REPO is already running ..."
 else
+  echo home directory is $HOME
   docker run -d -p 5492:5432 \
     --rm --name $CONTAINER_REPO \
     --health-cmd='stat /etc/passwd || exit 1' \
@@ -11,7 +12,7 @@ else
     -e POSTGRES_DB=velocorner \
     -e POSTGRES_USER=velocorner \
     -e POSTGRES_PASSWORD=velocorner \
-    -v /Users/levi/Downloads//velo/velocorner/psql/data:/var/lib/postgresql/data \
+    -v $HOME/Downloads/velo/velocorner/psql/data:/var/lib/postgresql/data \
     postgres:12.2-alpine
   echo "$CONTAINER_REPO has been started ..."
 fi
