10cda8050dfadcd6f48583bfadf0adff47d922c6
levi <velocorner.com@gmail.com>
2022-02-16 00:42:43 +0100

    search page for brands

--- web-app/public/javascripts/brands.js
+++ web-app/public/javascripts/brands.js
@@ -1,99 +1,49 @@
     function triggerSearch() {
         var query = $('#search_brand').val();
         console.log('searching for ' + query);
-        searchPage(null, query);
+        searchPage(query);
     }
 
-    function triggerSuggestion(suggestion) {
-        console.log('selected brand: ' + suggestion);
-        searchPage(suggestion, null);
-    }
-
-    // either suggested activity or queryToSearch
-    function searchPage(suggestedActivity, queryToSearch) {
-        // open search page if the table is not present
-        if ($('#activity-result').length <= 0) {
-            console.log('redirecting to the search page');
-            var queryParameter = suggestedActivity ? 'aid='+suggestedActivity.id : 'q='+queryToSearch;
-            window.open('search?'+queryParameter, '_self');
-        } else {
-            var activities = suggestedActivity ? Array.of(suggestedActivity) : null;
-            startSearch(activities, queryToSearch, null);
-        }
-    }
-
-    function startSearch(activities, queryToSearch, activityIdToSearch) {
-        // mark search in progress
-        $('#search-progress-bar').attr('aria-valuenow', 50);
-        $('#search-progress-bar').css('width', '50%');
-        $('#search-progress-bar').text('50% Searching...');
-        $('#search-progress').fadeIn();
+    // start searching for the query term
+    function searchPage(queryToSearch) {
+        // mark search in progress TODO
 
         // clean up existing entries
-        $('#activity-result tbody tr').remove();
+        $('#brand-result .inner-results').remove();
 
-        if (activities) {
-            // take suggested activity
-            finishSearch(activities);
-        } else {
-            var urlPath = queryToSearch ? "/api/brand/suggest?query=queryToSearch" : "/api/activities/"+activityIdToSearch
-            // trigger asynchronous search
-            if (queryToSearch) {
-                $.ajax({
-                    type: "GET",
-                    dataType: "json",
-                    url: "/api/brand/suggest?query="+queryToSearch,
-                    timeout: 20000,
-                    success: function(result) {
-                        finishSearch(result.suggestions.map(s => JSON.parse(s.data)));
-                    }
-                });
-            } else {
-                $.ajax({
-                    type: "GET",
-                    dataType: "json",
-                    url: "/api/brand/"+activityIdToSearch,
-                    timeout: 20000,
-                    success: function(result) {
-                        finishSearch(Array.of(result));
-                    }
-                });
+        // trigger asynchronous search
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "/api/brand/search?query="+queryToSearch,
+            timeout: 20000,
+            success: function(result) {
+                finishSearch(result);
             }
-        }
+        });
     }
 
-    function elapsedInSeconds(secs) {
-        var elapsed = moment.duration(secs, "seconds");
-        var tenthOfMins = elapsed.minutes() > 9 ? '' : '0';
-        return elapsed.hours()+'h:'+tenthOfMins+elapsed.minutes()+'m';
-    }
-
-    function wholeValueAndUnit(n, unit) {
-        return n ? n.toFixed(0) + ' ' + unit : ""
-    }
-
-    function finishSearch(activities) {
+    function finishSearch(markets) {
         // populate table with the results
-        console.log('populating with '+activities.length+' result(s)');
-        $.each(activities, function(ix, entry) {
-            $("#activity-result tbody").append(
-              '<tr><td><a href="http://www.strava.com/activities/'+entry.id+'">'+moment(entry.start_date_local).format('DD MMM, YYYY, ddd HH:mm A')+'</a></td>'+
-              '<td class="td-width"><a href="http://www.strava.com/activities/'+entry.id+'">'+entry.name+'</a></td>'+
-              '<td class="text-right">'+(entry.distance/1000).toFixed(1)+' km</td>'+
-              '<td class="text-right">'+wholeValueAndUnit(entry.total_elevation_gain, 'm')+'</td>'+
-              '<td>'+elapsedInSeconds(entry.moving_time)+' / '+elapsedInSeconds(entry.elapsed_time)+'</td>'+
-              '<td class="text-right">'+(entry.average_speed*3.6).toFixed(1)+' km/h</td>'+
-              '<td class="text-right">'+wholeValueAndUnit(entry.average_watts, 'W')+'</td>'+
-              '<td class="text-right">'+wholeValueAndUnit(entry.average_heartrate, 'hr')+'</td>'+
-              '<td class="text-right">'+wholeValueAndUnit(entry.average_temp, '˚C')+'</td>'+
-              '</tr>');
+        console.log('populating with '+markets.length+' result(s)');
+        var resultText = markets.length == 1 ? 'result' : 'results';
+        $('#results-number').text(markets.length+' '+resultText);
+
+        $.each(markets, function(ix, entry) {
+            $("#brand-result").append(
+              '<div class="inner-results">'+
+              '    <hr>'+
+              '    <h3><a href="'+entry.marketplace.url+'">'+entry.marketplace.name+'</a></h3>'+
+              '    <div class="overflow-h">'+
+              '       <img src="'+entry.marketplace.logoUrl+'" alt="">'+
+              '       <div class="overflow-a">'+
+              '           <a href="'+entry.url+'">'+entry.brand.name+'</a>'+
+              '       </div>'+
+              '    </div>'+
+              '<div>');
         });
 
-        // hide search activities
-        $('#search-progress-bar').attr('aria-valuenow', 100);
-        $('#search-progress-bar').css('width', '100%');
-        $('#search-progress-bar').text('Done');
-        $('#search-progress').fadeOut();
+        // hide search in progress TODO
     }
 
     $(document).ready(function() {
@@ -102,7 +52,7 @@
             searchField.autocomplete({
                 serviceUrl: '/api/brand/suggest',
                 onSelect: function (suggestion) {
-                    triggerSuggestion(suggestion.data);
+                    searchPage(suggestion.data);
                 }
             });
             $('#search_button_brand').click(function() {
@@ -114,11 +64,7 @@
                 }
             });
 
-            var urlParams = new URLSearchParams(window.location.search);
-            if ($('#brand-result') && (urlParams.has('aid') || urlParams.has('q'))) {
-                console.log('triggering search or suggestion');
-                searchField.val(urlParams.get('q') || "");
-                startSearch(null, urlParams.get('q'), urlParams.get('aid'));
-            }
+            // empty landing page, remove results counter
+            //$('#results-number').hide();
         }
     });
