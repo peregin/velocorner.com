32d6b91da63ccad147d8e7b1b62ce8ddb3304f8d
levi <velocorner.com@gmail.com>
2022-02-17 19:17:08 +0100

    update host to local [skip ci]

--- data-search/src/main/scala/velocorner/search/ElasticSupport.scala
+++ data-search/src/main/scala/velocorner/search/ElasticSupport.scala
@@ -1,12 +1,12 @@
 package velocorner.search
 
-import com.sksamuel.elastic4s.{ElasticClient, ElasticNodeEndpoint, ElasticProperties}
+import com.sksamuel.elastic4s.{ElasticClient, ElasticProperties}
 import com.sksamuel.elastic4s.http.JavaClient
 
 trait ElasticSupport {
 
   // TODO: extract it to config
-  private lazy val client = JavaClient(ElasticProperties("http://elasticsearch:9200"))
+  private lazy val client = JavaClient(ElasticProperties("http://localhost:9200"))
 
   def localCluster(): ElasticClient = ElasticClient(client)
 
