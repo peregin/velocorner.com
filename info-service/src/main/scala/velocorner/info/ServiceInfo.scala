c56c3dd495385941b1f655e202fe7a9731405488
levi <velocorner.com@gmail.com>
2022-04-11 08:23:56 +0200

    test [skip ci]

--- info-service/src/main/scala/velocorner/info/ServiceInfo.scala
+++ info-service/src/main/scala/velocorner/info/ServiceInfo.scala
@@ -7,7 +7,7 @@ import java.util.Calendar
 object ServiceInfo extends IOApp.Simple {
 
   def run: IO[Unit] = for {
-    _ <- IO(println(s"ServiceInfo: ${Calendar.getInstance().getTime()}"))
+    _ <- IO.println(s"ServiceInfo: ${Calendar.getInstance().getTime()}")
   } yield ()
 
 }
