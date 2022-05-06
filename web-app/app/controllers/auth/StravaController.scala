37d2e776ced96ea2c66a30cc129e320f5b87bb93
levi <velocorner.com@gmail.com>
2022-05-06 23:54:00 +0200

    prepare plain login function

--- web-app/app/controllers/auth/StravaController.scala
+++ web-app/app/controllers/auth/StravaController.scala
@@ -2,15 +2,17 @@ package controllers.auth
 
 import cats.implicits._
 import controllers.ConnectivitySettings
-import controllers.auth.StravaController.ec
+import controllers.auth.StravaController.{OAuth2StateKey, ec}
 import play.api.cache.SyncCacheApi
 import play.api.data.Form
-import play.api.data.Forms.{nonEmptyText, tuple, text}
+import play.api.data.Forms.{nonEmptyText, optional, text, tuple}
+import play.api.libs.json.{JsString, Json}
 import play.api.mvc._
 import velocorner.feed.OAuth2._
 import velocorner.model.Account
 import velocorner.model.strava.Gear
 
+import java.util.UUID
 import java.util.concurrent.Executors
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -24,6 +26,9 @@ object StravaController {
   type AuthenticityToken = String
   type ResultUpdater = Result => Result
 
+  // verifies the code between the session and submitted form
+  val OAuth2StateKey = "velocorner.oauth2.state"
+
   implicit val ec =
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10, (r: Runnable) => new Thread(r, "play worker") <| (_.setDaemon(true))))
 }
@@ -34,7 +39,7 @@ class StravaController @Inject() (val connectivity: ConnectivitySettings, val ca
 
   protected val authenticator: StravaAuthenticator = new StravaAuthenticator(connectivity)
 
-  // called from FE web-app
+  // called from FE web-app ONLY
   def login(scope: String) = timed("LOGIN") {
     Action { implicit request =>
       logger.info(s"LOGIN($scope)")
@@ -42,37 +47,47 @@ class StravaController @Inject() (val connectivity: ConnectivitySettings, val ca
         // already logged in
         case Some(_) => Redirect(controllers.routes.WebController.index)
         // authorize - scope is the host from FE - calls Strava with the callback url (authorize)
-        case None => Redirect(authenticator.getAuthorizationUrl(scope, none))
+
+        case None =>
+          val state = UUID.randomUUID().toString
+          Redirect(authenticator.getAuthorizationUrl(scope, state.some)).withSession(
+            request.session + (OAuth2StateKey -> state)
+          )
       }
     }
   }
 
   // callback from OAuth2 (triggered by Strava from web-app or web-front)
+  // creates the server side session and JWT token
+  // the caller must redirect to the proper page
   def authorize = Action.async { implicit request =>
     val form = Form(
       tuple(
         "code" -> nonEmptyText,
-        "state" -> text
+        "state" -> optional(text).verifying(_.forall(s => request.session.get(OAuth2StateKey).exists(_ == s)))
       )
     ).bindFromRequest()
     logger.info(s"authorize ${form.data}")
 
-    def formSuccess(v: (String, String)): Future[Result] = {
-      val (code, state) = v
-      for {
-        accessTokenResponse <- authenticator.retrieveAccessToken(code)
-        oauthResult <- loggedIn(request) match {
-          case Some(account) => onOAuthLinkSucceeded(accessTokenResponse, account)
-          case None          => onOAuthLoginSucceeded(accessTokenResponse)
-        }
-      } yield oauthResult
-    }
+    def formSuccess(code: String): Future[Result] = for {
+      accessTokenResponse <- authenticator.retrieveAccessToken(code)
+      _ = logger.info(s"OAUTH succeeded with access token[${accessTokenResponse.accessToken}]")
+      account <- login(accessTokenResponse, none)
+      jwtUser = JwtUser.toJwtUser(account)
+      sessionToken <- idContainer.startNewSession(account.athleteId, sessionTimeoutInSeconds)
+      jwt = jwtUser.toToken(connectivity.secretConfig.getJwtSecret)
+      result = Ok(Json.obj("token" -> JsString(jwt)))
+      _ = tokenAccessor.put(sessionToken)(result)
+    } yield result
 
-    form.value match {
-      // TODO: testing headers in response
-      case Some(v) if !form.hasErrors => formSuccess(v).map(_.withHeaders("jwt" -> "hello"))
-      case _                          => Future.successful(BadRequest)
+    val result = form.value match {
+      case Some(v) if !form.hasErrors =>
+        val (code, _) = v
+        formSuccess(code)
+      case _ =>
+        Future.successful[Result](BadRequest)
     }
+    result.map(_.removingFromSession(OAuth2StateKey))
   }
 
   def logout = Action { implicit request =>
@@ -104,14 +119,16 @@ class StravaController @Inject() (val connectivity: ConnectivitySettings, val ca
   def onOAuthLoginSucceeded(resp: OAuth2TokenResponse)(implicit ctx: ExecutionContext): Future[Result] = {
     logger.info(s"oauth LOGIN succeeded with token[${resp.accessToken}]")
     for {
-      athlete <- login(resp, none)
-      token <- idContainer.startNewSession(athlete.id, sessionTimeoutInSeconds)
+      account <- login(resp, none)
+      token <- idContainer.startNewSession(account.athleteId, sessionTimeoutInSeconds)
       result <- Future.successful(Redirect(controllers.routes.WebController.index))
     } yield tokenAccessor.put(token)(result)
   }
 
   // same functionality when the account is linked with a given consumerUser or logged in to an existing mapping
-  private def login(resp: OAuth2TokenResponse, consumerUser: Option[ConsumerUser])(implicit ctx: ExecutionContext): Future[ProviderUser] =
+  // consumerUser - when empty means a new login
+  // consumerUser - when present links the login with the given account
+  private def login(resp: OAuth2TokenResponse, consumerUser: Option[ConsumerUser])(implicit ctx: ExecutionContext): Future[Account] =
     for {
       // retrieve the provider user has more details than the user got at authentication
       athlete <- retrieveProviderUser(resp.accessToken)
@@ -130,5 +147,5 @@ class StravaController @Inject() (val connectivity: ConnectivitySettings, val ca
       _ <- storage.getAccountStorage.store(account)
       _ <- athlete.bikes.getOrElse(Nil).traverse(storage.getGearStorage.store(_, Gear.Bike))
       _ <- athlete.shoes.getOrElse(Nil).traverse(storage.getGearStorage.store(_, Gear.Shoe))
-    } yield athlete
+    } yield account
 }
