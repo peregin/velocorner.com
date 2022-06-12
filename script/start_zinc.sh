26464e8269a42656730729c91d4b689dc0cfae78
levi <velocorner.com@gmail.com>
2022-06-12 22:41:59 +0200

    add zinc [skip ci]

---
+++ script/start_zinc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+CONTAINER_REPO="zinc"
+if [[ $(docker inspect -f '{{.State.Running}}' $CONTAINER_REPO) = "true" ]]; then
+  echo "$CONTAINER_REPO is already running ..."
+else
+  echo "starting $CONTAINER_REPO ..."
+  docker run -d --rm --name $CONTAINER_REPO \
+      -v $HOME/Downloads/velo/velocorner/zinc/data:/data \
+      -e ZINC_DATA_PATH="/data" \
+      -e ZINC_FIRST_ADMIN_USER=admin -e ZINC_FIRST_ADMIN_PASSWORD=admin \
+      -p 4080:4080 \
+      public.ecr.aws/zinclabs/zinc:latest
+  echo "$CONTAINER_REPO has been started ..."
+fi
