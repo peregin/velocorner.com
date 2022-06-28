bff8aa0114ed48c62b06569231051c1e69515570
levi <velocorner.com@gmail.com>
2022-06-28 23:29:41 +0200

    bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val dockerBaseImage = "openjdk:11-jre-slim-buster"
 
   val catsVersion = "2.8.0"
-  val catsEffectVersion = "3.3.12"
+  val catsEffectVersion = "3.3.13"
   val mouseVersion = "1.1.0"
   val fs2Version = "3.2.8"
   val zioVersion = "2.0.0"
