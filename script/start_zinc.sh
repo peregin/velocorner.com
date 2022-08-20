db1ae473a1aab2359d06e0de4a5bd46fe651822e
levi <velocorner.com@gmail.com>
2022-08-20 22:40:14 +0200

    use dedicated version of zinc

--- script/start_zinc.sh
+++ script/start_zinc.sh
@@ -10,6 +10,6 @@ else
       -e ZINC_DATA_PATH="/data" \
       -e ZINC_FIRST_ADMIN_USER=admin -e ZINC_FIRST_ADMIN_PASSWORD=admin \
       -p 4080:4080 \
-      public.ecr.aws/zinclabs/zinc:latest
+      public.ecr.aws/zinclabs/zinc:0.2.5
   echo "$CONTAINER_REPO has been started ..."
 fi
