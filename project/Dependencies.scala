b028f7bc79229dbb60f0c0bf7faac243d8250f5e
levi <velocorner.com@gmail.com>
2022-09-01 07:40:41 +0200

    bump deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
 
   val dockerBaseImage = "openjdk:11-jre-slim-buster"
 
+  val flywayVersion = "9.2.1"
   val catsVersion = "2.8.0"
   val catsEffectVersion = "3.3.14"
   val mouseVersion = "1.1.0"
@@ -13,16 +14,15 @@ object Dependencies {
   val finatraVersion = "21.2.0"
   val argonautVersion = "6.3.8"
   val playWsVersion = "2.1.10" // standalone version
-  val playJsonVersion = "2.9.2"
+  val playJsonVersion = "2.9.3"
   val shapelessVersion = "2.3.9"
   val logbackVersion = "1.4.0"
-  val flywayVersion = "9.2.0"
   val doobieVersion = "1.0.0-RC2"
   val orientDbVersion = "3.2.9"
   val mongoDbVersion = "4.7.1"
   val rethinkDbVersion = "2.4.4"
-  val elasticVersion = "8.4.0"
-  val jwtVersion = "9.1.0"
+  val elasticVersion = "8.4.1"
+  val jwtVersion = "9.1.1"
   val squantsVersion = "1.8.3"
   val sparkVersion = "3.3.0"
   val scalaTestVersion = "3.2.13"
