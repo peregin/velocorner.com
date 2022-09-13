386dc7b92f2e12b89bc51ba44fa060c94d8477ea
levi <velocorner.com@gmail.com>
2022-09-13 18:46:49 +0200

    bump shapeless version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val argonautVersion = "6.3.8"
   val playWsVersion = "2.1.10" // standalone version
   val playJsonVersion = "2.9.3"
-  val shapelessVersion = "2.3.9"
+  val shapelessVersion = "2.3.10"
   val logbackVersion = "1.4.0"
   val doobieVersion = "1.0.0-RC2"
   val orientDbVersion = "3.2.9"
