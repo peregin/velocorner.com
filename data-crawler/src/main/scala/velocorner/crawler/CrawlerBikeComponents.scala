c993fd91eb85266096dc738a319a07f2c1a065cd
levi <velocorner.com@gmail.com>
2022-09-17 21:34:12 +0200

    map BE - adapt router - wip

--- data-crawler/src/main/scala/velocorner/crawler/CrawlerBikeComponents.scala
+++ data-crawler/src/main/scala/velocorner/crawler/CrawlerBikeComponents.scala
@@ -10,7 +10,7 @@ import org.http4s.{Method, Uri}
 import org.http4s.client.dsl.Http4sClientDsl
 import velocorner.api.Money
 import velocorner.api.brand.Marketplace.BikeComponents
-import velocorner.api.brand.{Marketplace, ProductDetails}
+import velocorner.api.brand.{Brand, Marketplace, ProductDetails}
 import velocorner.crawler.CrawlerBikeComponents._
 
 import java.net.URLEncoder
@@ -18,12 +18,22 @@ import java.net.URLEncoder
 object CrawlerBikeComponents {
 
   // the BC specific request and responses
-  case class SuggestProduct(name: String, price: String, description: String)
+  case class SuggestImage(path: String, mimeType: String)
+  object SuggestImage {
+    implicit val codec: Codec[SuggestImage] = deriveCodec
+  }
+
+  case class SuggestProduct(name: String, price: String, description: String, imageMedium: SuggestImage, link: String, manufacturer: String)
   object SuggestProduct {
     implicit val codec: Codec[SuggestProduct] = deriveCodec
   }
 
-  case class Suggest(products: List[SuggestProduct])
+  case class SuggestManufacturer(name: String, image: String)
+  object SuggestManufacturer {
+    implicit val codec: Codec[SuggestManufacturer] = deriveCodec
+  }
+
+  case class Suggest(products: List[SuggestProduct], manufacturers: List[SuggestManufacturer])
   object Suggest {
     implicit val codec: Codec[Suggest] = deriveCodec
   }
@@ -32,18 +42,20 @@ object CrawlerBikeComponents {
     def toApi(): List[ProductDetails] = suggestions.products.map { p =>
       ProductDetails(
         market = BikeComponents,
-        brand = none, // : Option[Brand],
+        brand = Brand(name = p.manufacturer, logoUrl = none).some,
         name = p.name,
         description = p.description.some,
         price = extractPrice(p.price),
-        imageUrl = "todo",
-        productUrl = "todo"
+        imageUrl = p.imageMedium.path,
+        productUrl = p.link
       )
     }
   }
   object SuggestResponse {
     implicit val codec: Codec[SuggestResponse] = deriveCodec
   }
+
+  // ---------------- utility functions ----------------
 
   val pricePattern = "([\\d\\s.,]*\\d)\\s*(\\S*)".r
 
--- data-crawler/src/main/scala/velocorner/crawler/Router.scala
+++ data-crawler/src/main/scala/velocorner/crawler/Router.scala
@@ -1,13 +1,18 @@
 package velocorner.crawler
 
-import cats.effect.Concurrent
+import cats.implicits._
+import cats.Parallel
+import cats.effect.Async
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
-class Router[F[_]: Concurrent](crawlers: List[Crawler[F]]) extends Http4sDsl[F] {
+class Router[F[_]: Async: Parallel](crawlers: List[Crawler[F]]) extends Http4sDsl[F] {
 
   val routes: HttpRoutes[F] = HttpRoutes
     .of[F] { case GET -> Root / "search" / term =>
-      Ok(s"Hello, looking for $term")
+      for {
+        suggestions <- crawlers.parTraverse(_.products(term))
+        res2 <- Ok(s"Hello, looking for $term")
+      } yield res2
     }
 }
