c2177f0ebd8b64ec4b4a913ebb419104f17770ce
Levente Fall <levente.fall@tundra.com>
2022-09-19 21:55:49 +0200

    set decoders - wip [skip ci]

--- data-crawler/src/main/scala/velocorner/crawler/Router.scala
+++ data-crawler/src/main/scala/velocorner/crawler/Router.scala
@@ -1,18 +1,31 @@
 package velocorner.crawler
 
-import cats.implicits._
 import cats.Parallel
 import cats.effect.Async
+import cats.implicits._
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
 import org.http4s.HttpRoutes
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.Http4sDsl
+import org.typelevel.log4cats.Logger
+import velocorner.api.Money
+import velocorner.api.brand.{Brand, Marketplace, ProductDetails}
 
-class Router[F[_]: Async: Parallel](crawlers: List[Crawler[F]]) extends Http4sDsl[F] {
+class Router[F[_]: Async: Parallel: Logger](crawlers: List[Crawler[F]]) extends Http4sDsl[F] {
+
+  implicit val codecMarket: Codec[Marketplace] = deriveCodec
+  implicit val codecMoney: Codec[Money] = deriveCodec
+  implicit val codecBrand: Codec[Brand] = deriveCodec
+  implicit val codec: Codec[ProductDetails] = deriveCodec
 
   val routes: HttpRoutes[F] = HttpRoutes
     .of[F] { case GET -> Root / "search" / term =>
       for {
-        suggestions <- crawlers.parTraverse(_.products(term))
-        res2 <- Ok(s"Hello, looking for $term")
-      } yield res2
+        _ <- Logger[F].info(s"searching for $term...")
+        suggestions <- crawlers.parTraverse(_.products(term)).map(_.flatten)
+        _ <- Logger[F].info(s"found ${suggestions.size} results...")
+        res <- Ok(suggestions)
+      } yield res
     }
 }
