ce7ec8773e7c3d4b373278025741c18f976c0594
levi <velocorner.com@gmail.com>
2022-10-28 20:22:08 +0200

    cleanup

--- web-front/src/service/ApiClient.js
+++ web-front/src/service/ApiClient.js
@@ -1,10 +1,10 @@
-
 const apiHost = process.env.API_HOST || 'http://localhost:9001'
 
 function getStatus(cb) {
   const requestOptions = {
     method: 'GET',
-    headers: authHeader()
+    mode: 'no-cors',
+    accept: "application/json"
   }
   return fetch(apiHost + '/api/status', requestOptions)
     .then(checkStatus)
@@ -13,24 +13,21 @@ function getStatus(cb) {
 }
 
 async function test() {
-  let url = 'https://www.strava.com/api/v3/oauth/authorize?client_id=4486&redirect_uri=http%3A%2F%2Flocalhost%3A9001%2Fauthorize%2Fstrava&response_type=code&approval_prompt=auto&scope=read%2Cactivity%3Aread'
+  //let url = 'https://www.strava.com/api/v3/oauth/authorize?client_id=4486&redirect_uri=http%3A%2F%2Flocalhost%3A9001%2Fauthorize%2Fstrava&response_type=code&approval_prompt=auto&scope=read%2Cactivity%3Aread'
   //let resp = window.location = url
-  const requestOptions = {
-    method: 'POST',
-    accept: "application/json",
-    redirect: 'follow',
-    credential: 'include',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    mode: 'no-cors'
-  }
+  // const requestOptions = {
+  //   method: 'POST',
+  //   accept: "application/json",
+  //   redirect: 'follow',
+  //   credential: 'include',
+  //   headers: {
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   },
+  //   mode: 'no-cors'
+  //}
   //let resp = await fetch(url, requestOptions)
   //  .then(res => console.log(`LOGIN response[${res}]`))
   //  .catch(console.error);
-
-  let resp = window.location.href = url;
-  console.log(`AUTH response is ${resp}`);
 }
 
 function login() {
@@ -44,18 +41,6 @@ function login() {
     .then(a => console.log(`LOGIN response[${a}]`))
 }
 
-// Utility functions
-function authHeader() {
-  // return authorization header with jwt token
-  const currentUser = {
-    token: 'jwtToken'
-  }
-  if (currentUser && currentUser.token) {
-    return { 'Authorization': `Bearer ${currentUser.token}` }
-  } else {
-    return {}
-  }
-}
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
