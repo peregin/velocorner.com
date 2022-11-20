5e07a996c7da660c3b25bf832d6776b86e916c00
Levente Fall <levente.fall@gmail.com>
2022-11-20 14:16:35 +0100

    use hash router

--- web-front/src/App.js
+++ web-front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter, Route, Routes,
+  HashRouter, Route, Routes,
 } from 'react-router-dom';
 
 import { ChakraProvider } from '@chakra-ui/react'
@@ -18,13 +18,13 @@ const App = () => {
 
   return (
     <ChakraProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<div><Header /><Home /><Footer /></div>} />
           <Route exact path="/best" element={<Best />} />
           <Route path="/about" element={<div><Header /><About /><Footer /></div>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   )
 }
