25736be7fa30d3ebaff314a23ffaf5ec7955f389
levi <velocorner.com@gmail.com>
2022-11-29 22:04:36 +0100

    bump ce version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val flywayVersion = "9.8.3"
   val catsVersion = "2.9.0"
-  val catsEffectVersion = "3.4.1"
+  val catsEffectVersion = "3.4.2"
   val mouseVersion = "1.2.1"
   val fs2Version = "3.4.0"
   val zioVersion = "2.0.4"
