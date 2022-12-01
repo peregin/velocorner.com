89cd41842e59260207ff2caffc7207cde41fc3d2
Levente Fall <levente.fall@gmail.com>
2022-12-01 13:58:16 +0100

    thank you Peter

--- web-front/src/components/SupportedMarkets.jsx
+++ web-front/src/components/SupportedMarkets.jsx
@@ -2,8 +2,7 @@ import { React, useEffect, useState } from "react";
 import { LinkBox, LinkOverlay, Text, Image } from "@chakra-ui/react";
 import ApiClient from "../service/ApiClient";
 
-const SupportedMarkets = ({ defIx = 1, timing = 1000 }) => {
-
+const SupportedMarkets = ({ defIx = 1, timing = 5000 }) => {
   const [data, setData] = useState([]);
   const [ix, setIx] = useState(defIx);
 
@@ -12,25 +11,41 @@ const SupportedMarkets = ({ defIx = 1, timing = 1000 }) => {
       let no = markets.length;
       console.info(`supporting ${no} markets`);
       setData(markets);
-
-      // setInterval(() => {
-      //   let newIx = 0;
-      //   if (ix < no - 1) {
-      //     newIx = ix + 1;
-      //   }
-      //   //console.info(`setting market to ${newIx} out of ${no}`)
-      //   setIx(newIx);
-      // }, timing);
     });
   }, []);
 
-  return data.length > 0 && (
-    <LinkBox maxW='sm' p='10' borderWidth='0' borderRadius='lg' overflow='hidden'>
-      <LinkOverlay href={data[ix].url}></LinkOverlay>
-      <Image src={data[ix].logoUrl} h='30px'/>
-      <Text align='center' fontWeight='bold' letterSpacing='wide'>{data[ix].name}</Text>
-    </LinkBox>
-  )
-}
+  useEffect(() => {
+    let id = setInterval(() => {
+      setIx((oldIx) => {
+        let newIx = 0;
+        let no = data.length;
+        if (oldIx < no - 1) {
+          newIx = oldIx + 1;
+        }
+        //console.info(`setting market to ${newIx} out of ${no}`);
+        return newIx;
+      });
+    }, timing);
+    return () => clearInterval(id);
+  }, [data, timing]);
+
+  return (
+    data.length > 0 && (
+      <LinkBox
+        maxW="sm"
+        p="10"
+        borderWidth="0"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <LinkOverlay href={data[ix].url}></LinkOverlay>
+        <Image src={data[ix].logoUrl} h="30px" />
+        <Text align="center" fontWeight="bold" letterSpacing="wide">
+          {data[ix].name}
+        </Text>
+      </LinkBox>
+    )
+  );
+};
 
 export default SupportedMarkets;
--- web-front/src/pages/Best.js
+++ web-front/src/pages/Best.js
@@ -1,93 +1,128 @@
-import { React, useState, useEffect } from "react";
-import { Flex, Heading, Input, IconButton, Box, Text, Progress, SimpleGrid } from "@chakra-ui/react";
+import { React, useState, useEffect, useCallback } from "react";
+import {
+  Flex,
+  Heading,
+  Input,
+  IconButton,
+  Box,
+  Text,
+  Progress,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import ApiClient from "../service/ApiClient";
 import ProductCard from "../components/ProductCard";
 import SupportedMarkets from "../components/SupportedMarkets";
 
 const Best = () => {
+  const [input, setInput] = useState("");
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [elapsed, setElapsed] = useState("");
+  const [searching, setSearching] = useState(false);
 
-  const [input, setInput] = useState('')
-  const [query, setQuery] = useState('')
-  const [results, setResults] = useState([])
-  const [elapsed, setElapsed] = useState('')
-  const [searching, setSearching] = useState(false)
-
-  useEffect(() => {
-    fetchData()
-  }, [query])
-
-  const fetchData = async () => {
-    if (query.trim === '') {
-      return
+  const fetchData = useCallback(async () => {
+    if (query.trim === "") {
+      return;
     }
     let startTime = new Date().getTime();
 
-    setSearching(true)
-    let results = await ApiClient.search(query) // todo: .catch(error...)
-    setSearching(false)
+    setSearching(true);
+    let results = await ApiClient.search(query); // todo: .catch(error...)
+    setSearching(false);
 
-    let elapsedTime = new Date().getTime() - startTime
-    let took = (elapsedTime / 1000).toFixed(2)
+    let elapsedTime = new Date().getTime() - startTime;
+    let took = (elapsedTime / 1000).toFixed(2);
     if (results.length > 0)
-      setElapsed(results.length+' results in '+took+' seconds')
-    else
-      setElapsed('')
+      setElapsed(results.length + " results in " + took + " seconds");
+    else setElapsed("");
 
-    console.info(results.length + ' results')
-    setResults(results)
-  }
+    console.info(results.length + " results");
+    setResults(results);
+  }, [query]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   const handleEnter = (ev) => {
-    if (ev.key === 'Enter') {
-      console.info('enter searching for ' + input + '...')
-      setQuery(input)
+    if (ev.key === "Enter") {
+      console.info("enter searching for " + input + "...");
+      setQuery(input);
     }
-  }
+  };
 
   const handleSearch = (ev) => {
-    console.info('click searching for ' + input + '...')
-    setQuery(input)
-  }
+    console.info("click searching for " + input + "...");
+    setQuery(input);
+  };
 
   return (
-      <Flex align="center" gap="5" direction="column" margin={[5, 10]}>
-        <Heading as='h4' size={['sm', 'md']}>
-          Find the best price for bikes, components, accessories, clothing,
-          brands and items 🎉
-        </Heading>
+    <Flex align="center" gap="5" direction="column" margin={[5, 10]}>
+      <Heading as="h4" size={["sm", "md"]}>
+        Find the best price for bikes, components, accessories, clothing, brands
+        and items 🎉
+      </Heading>
 
-        <Box w="100%" border='1px' borderColor='lightgray' borderLeft='2px' borderLeftColor='green' pl={[2, 20]} pr={[2, 20]} pb={[2, 5]}>
-        <Text m='5' fontSize={['sm', '1.2em']}>Search for the best price on your favorite brands, components, bicycles and accessories:</Text>
-          <Flex>
-            <Input value={input} onChange={(ev) => setInput(ev.target.value)} onKeyDown={handleEnter} placeholder='Type in your query...'/>
-            <IconButton colorScheme="green" aria-label="Search components" icon={<SearchIcon onClick={handleSearch} />}/>
-          </Flex>
-        </Box>
+      <Box
+        w="100%"
+        border="1px"
+        borderColor="lightgray"
+        borderLeft="2px"
+        borderLeftColor="green"
+        pl={[2, 20]}
+        pr={[2, 20]}
+        pb={[2, 5]}
+      >
+        <Text m="5" fontSize={["sm", "1.2em"]}>
+          Search for the best price on your favorite brands, components,
+          bicycles and accessories:
+        </Text>
+        <Flex>
+          <Input
+            value={input}
+            onChange={(ev) => setInput(ev.target.value)}
+            onKeyDown={handleEnter}
+            placeholder="Type in your query..."
+          />
+          <IconButton
+            colorScheme="green"
+            aria-label="Search components"
+            icon={<SearchIcon onClick={handleSearch} />}
+          />
+        </Flex>
+      </Box>
 
-        <Progress w='100%' hasStripe isIndeterminate visibility={searching ? 'visible' : 'hidden'}/>
-        {results.length > 0 && <Text color='gray'>{elapsed}</Text>}
+      <Progress
+        w="100%"
+        hasStripe
+        isIndeterminate
+        visibility={searching ? "visible" : "hidden"}
+      />
+      {results.length > 0 && <Text color="gray">{elapsed}</Text>}
 
-        <SimpleGrid columns={{sm: 2, md: 4}} spacing={[2, 10]}>
-          {results.map( (res, ix) => {
-            return <ProductCard 
+      <SimpleGrid columns={{ sm: 2, md: 4 }} spacing={[2, 10]}>
+        {results.map((res, ix) => {
+          return (
+            <ProductCard
               key={ix}
               productName={res.name}
               productUrl={res.productUrl}
               brandName={res.brand?.name}
               marketName={res.market.name}
-              formattedPrice={res.price.value + ' ' + res.price.currency}
+              formattedPrice={res.price.value + " " + res.price.currency}
               imageUrl={res.imageUrl}
               imageAlt={res.name}
               reviewStars={res.reviewStars}
               isNew={res.isNew}
             />
-          })}
-        </SimpleGrid>
+          );
+        })}
+      </SimpleGrid>
 
-        <SupportedMarkets defIx={Math.floor(Math.random() * 5)}/>
-      </Flex>
-    )
-}
+      <SupportedMarkets defIx={Math.floor(Math.random() * 5)} />
+    </Flex>
+  );
+};
 
 export default Best;
