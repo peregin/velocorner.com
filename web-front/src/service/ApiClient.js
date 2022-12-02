3821a3a32c5028d87e4cbca8429cdffa8b52ddad
levi <velocorner.com@gmail.com>
2022-12-02 19:43:12 +0100

    updates + cors

--- web-front/src/service/ApiClient.js
+++ web-front/src/service/ApiClient.js
@@ -2,32 +2,32 @@
 const apiHost = process.env.API_HOST || process.env.REACT_APP_API_HOST || 'https://velocorner.com'
 console.info('api host: ' + apiHost)
 
-const requestOptions = {
+const requestOptionsGet = {
   method: 'GET',
   accept: 'application/json',
   cache: 'no-cache'
 }
 
 function status() {
-  return fetch(apiHost + '/api/status', requestOptions)
+  return fetch(apiHost + '/api/status', requestOptionsGet)
     .then(checkStatus)
     .then(r => r.json())
 }
 
 function search(term) {
-  return fetch(apiHost + '/api/products/search?query=' + term, requestOptions)
+  return fetch(apiHost + '/api/products/search?query=' + term, requestOptionsGet)
     .then(checkStatus)
     .then(r => r.json())
 }
 
 function markets() {
-  return fetch(apiHost + '/api/products/markets', requestOptions)
+  return fetch(apiHost + '/api/products/markets', requestOptionsGet)
     .then(checkStatus)
     .then(r => r.json())
 }
 
 function login() {
-  return fetch('/api/login/strava', requestOptions)
+  return fetch('/api/login/strava', requestOptionsGet)
     .then(checkStatus)
     .then(r => r.text())
     .then(a => console.log(`LOGIN response[${a}]`))
