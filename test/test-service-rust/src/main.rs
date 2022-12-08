74086774bf23bca13721c39d16f3b76f5592e60f
Levente Fall <levente.fall@gmail.com>
2022-12-08 22:03:05 +0100

    bump deps and learning Rust

---
+++ test/test-service-rust/src/main.rs
@@ -0,0 +1,14 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("aloha");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
