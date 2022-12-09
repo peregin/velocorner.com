bdde4d11251a2ab1d75094e9ff22ea4026145363
Levente Fall <levente.fall@gmail.com>
2022-12-09 06:18:46 +0100

    test web client in rust [skip ci]

--- test/test-service-rust/src/main.rs
+++ test/test-service-rust/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc;
 use std::thread;
+use actix_web::client;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
@@ -11,4 +12,14 @@ fn main() {
 
     let received = rx.recv().unwrap();
     println!("Got: {}", received);
+
+    client::get("http://www.rust-lang.org")   // <- Create request builder
+        .header("User-Agent", "Actix-web")
+        .finish().unwrap()
+        .send()                               // <- Send http request
+        .map_err(|_| ())
+        .and_then(|response| {                // <- server http response
+            println!("Response: {:?}", response);
+            Ok(())
+        });
 }
