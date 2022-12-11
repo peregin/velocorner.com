b350fa7634e370bf410c9c7417dad4abcc626326
levi <velocorner.com@gmail.com>
2022-12-11 22:13:12 +0100

    rates [skip ci]

--- test/test-service-rust/src/main.rs
+++ test/test-service-rust/src/main.rs
@@ -1,13 +1,13 @@
-use std::iter::Map;
 use std::sync::mpsc;
 use std::thread;
 use awc::Client;
 use serde::{Serialize, Deserialize};
+use std::{default::Default, collections::HashMap};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Response {
     base: String,
-    //rates: Map<String, String>
+    rates: HashMap<String, f32>
 }
 
 #[actix_web::main]
@@ -21,14 +21,15 @@ async fn main() {
     println!("Got: {received}");
 
     let client = Client::default();
-    let reply = client.get("https://api.exchangerate.host/latest")
+    let mut reply = client.get("https://api.exchangerate.host/latest")
         .insert_header(("User-Agent", "actix-web"))
         .insert_header(("Content-Type", "application/json"))
         .send()                               // <- Send http request
-        .await;
+        .await
+        .unwrap();
 
-    let mut payload = reply.unwrap();
-    println!("status={:?}, ratelimit-remaining={:?}", payload.status(), payload.headers().get("x-ratelimit-remaining"));
-    let json = payload.json::<Response>().await;
+    //let mut payload = reply.unwrap();
+    println!("status={:?}, ratelimit-remaining={:?}", reply.status(), reply.headers().get("x-ratelimit-remaining"));
+    let json = reply.json::<Response>().await.unwrap();
     println!("json = {:?}", json);
 }
