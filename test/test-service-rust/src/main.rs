56cbff1bf89e14ee6aa370d0b339c0d004dd9ad2
levi <velocorner.com@gmail.com>
2022-12-11 21:36:16 +0100

    ger rates with rust [skip ci]

--- test/test-service-rust/src/main.rs
+++ test/test-service-rust/src/main.rs
@@ -1,23 +1,34 @@
+use std::iter::Map;
 use std::sync::mpsc;
 use std::thread;
 use awc::Client;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    base: String,
+    //rates: Map<String, String>
+}
 
 #[actix_web::main]
 async fn main() {
     let (tx, rx) = mpsc::channel();
-
     thread::spawn(move || {
         let val = String::from("aloha");
         tx.send(val).unwrap();
     });
-
     let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    println!("Got: {received}");
+
     let client = Client::default();
-    let res = client.get("http://www.rust-lang.org")   // <- Create request builder
-        .insert_header(("User-Agent", "Actix-web"))
+    let reply = client.get("https://api.exchangerate.host/latest")
+        .insert_header(("User-Agent", "actix-web"))
+        .insert_header(("Content-Type", "application/json"))
         .send()                               // <- Send http request
         .await;
 
-    println!("res = {:?}", res);
+    let mut payload = reply.unwrap();
+    println!("status={:?}, ratelimit-remaining={:?}", payload.status(), payload.headers().get("x-ratelimit-remaining"));
+    let json = payload.json::<Response>().await;
+    println!("json = {:?}", json);
 }
