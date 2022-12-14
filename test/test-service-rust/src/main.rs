51b7a541381fd021503aed98933fe91987a9be51
Levente Fall <levente.fall@gmail.com>
2022-12-14 08:08:22 +0100

    test web service/client in rust [skip ci]

--- test/test-service-rust/src/main.rs
+++ test/test-service-rust/src/main.rs
@@ -1,35 +1,54 @@
-use std::sync::mpsc;
-use std::thread;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use awc::Client;
-use serde::{Serialize, Deserialize};
-use std::{default::Default, collections::HashMap};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, default::Default};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Response {
+struct ExchangeRate {
     base: String,
-    rates: HashMap<String, f32>
+    rates: HashMap<String, f32>,
 }
 
-#[actix_web::main]
-async fn main() {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let val = String::from("aloha");
-        tx.send(val).unwrap();
-    });
-    let received = rx.recv().unwrap();
-    println!("Got: {received}");
-
+async fn live() -> ExchangeRate {
     let client = Client::default();
-    let mut reply = client.get("https://api.exchangerate.host/latest")
+    let mut reply = client
+        .get("https://api.exchangerate.host/latest?base=CHF")
         .insert_header(("User-Agent", "actix-web"))
         .insert_header(("Content-Type", "application/json"))
-        .send()                               // <- Send http request
+        .send()
         .await
         .unwrap();
 
     //let mut payload = reply.unwrap();
-    println!("status={:?}, ratelimit-remaining={:?}", reply.status(), reply.headers().get("x-ratelimit-remaining"));
-    let json = reply.json::<Response>().await.unwrap();
+    println!(
+        "status={:?}, ratelimit-remaining={:?}",
+        reply.status(),
+        reply.headers().get("x-ratelimit-remaining")
+    );
+    let json = reply.json::<ExchangeRate>().await.unwrap();
     println!("json = {:#?}", json);
+    json
+}
+
+async fn welcome(_: HttpRequest) -> impl Responder {
+    let now: DateTime<Utc> = Utc::now();
+    format!("Welcome to exchange rate service, {now}")
+}
+
+async fn rates(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(welcome))
+            .route("/rates", web::get().to(rates))
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
