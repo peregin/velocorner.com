e87037eea9d3c80a7bea603d984b299e43ddf8da
Scala Steward <scala_steward@virtuslab.com>
2023-01-17 20:08:47 +0000

    Update fs2-core, fs2-io to 3.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val catsVersion = "2.9.0"
   val catsEffectVersion = "3.4.4"
   val mouseVersion = "1.2.1"
-  val fs2Version = "3.4.0"
+  val fs2Version = "3.5.0"
   val zioVersion = "2.0.5"
   val http4s = "0.23.17"
   val finatraVersion = "21.2.0"
