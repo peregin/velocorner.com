e7017d566ab13656c72ca306b9f5eb40f3baa254
levi <velocorner.com@gmail.com>
2023-01-30 22:32:20 +0100

    scaffolding

--- weather-service/src/main/kotlin/velocorner/weather/Main.kt
+++ weather-service/src/main/kotlin/velocorner/weather/Main.kt
@@ -1,18 +1,30 @@
 package velocorner.weather
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+// entry point defined in config
 fun Application.module() {
-//    configureRouting()
+    configureRouting()
     configureSerialization()
 }
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
+    }
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello!", ContentType.Text.Html)
+        }
     }
 }
