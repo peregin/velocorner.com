6ead7f8c964b411639c5266145269369d3d84b19
levi <velocorner.com@gmail.com>
2023-02-03 00:08:53 +0100

    add docker plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     kotlin("jvm") version "1.8.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
     id("name.remal.check-updates") version "1.5.0"
+    id("io.ktor.plugin") version "2.2.3"
 }
 
 application {
@@ -30,4 +31,31 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
+
+ktor {
+    fatJar {
+        archiveFileName.set("service.jar")
+    }
+
+    docker {
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
+        localImageName.set("velocorner.weather")
+        imageTag.set("latest")
+        portMappings.set(listOf(
+            io.ktor.plugin.features.DockerPortMapping(
+                9015,
+                9015,
+                io.ktor.plugin.features.DockerPortMappingProtocol.TCP
+            )
+        ))
+
+        externalRegistry.set(
+            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
+                appName = provider { "ktor-app" },
+                username = providers.environmentVariable("DOCKER_HUB_USERNAME"),
+                password = providers.environmentVariable("DOCKER_HUB_PASSWORD")
+            )
+        )
+    }
 }
