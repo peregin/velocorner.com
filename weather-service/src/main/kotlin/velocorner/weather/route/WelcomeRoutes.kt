aac8757c2ecdf756eb90e74428990da86079406e
levi <velocorner.com@gmail.com>
2023-02-02 23:56:27 +0100

    respond with current time

--- weather-service/src/main/kotlin/velocorner/weather/route/WelcomeRoutes.kt
+++ weather-service/src/main/kotlin/velocorner/weather/route/WelcomeRoutes.kt
@@ -4,9 +4,12 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun Route.welcomeRoutes() {
     get("/") {
-        call.respondText("Aloha!", ContentType.Text.Html)
+        val now = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME)
+        call.respondText("Welcome @ $now", ContentType.Text.Html)
     }
 }
