d204b6017f6c5db53a3a78f8a180c3359bd902e1
levi <velocorner.com@gmail.com>
2023-02-03 10:14:08 +0100

    add html dsl for the landing page

--- src/main/kotlin/velocorner/weather/route/WelcomeRoutes.kt
+++ src/main/kotlin/velocorner/weather/route/WelcomeRoutes.kt
@@ -2,14 +2,23 @@ package velocorner.weather.route
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import io.ktor.server.html.*
+import kotlinx.html.*
 
 fun Route.welcomeRoutes() {
     get("/") {
         val now = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME)
-        call.respondText("Welcome @ $now", ContentType.Text.Html)
+        call.respondHtml(HttpStatusCode.OK) {
+            body {
+                h1 { +"Welcome @ $now" }
+                ul {
+                    li { a("weather/current") { +"current weather" } }
+                    li { a("weather/forecast") { +"5 days forecast" } }
+                }
+            }
+        }
     }
 }
