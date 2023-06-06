f3ef04ab83f6e28d5f2521ef983c3d51c6887c12
levi <velocorner.com@gmail.com>
2023-06-06 22:45:19 +0200

    add type annotation

--- data-provider/src/main/scala/velocorner/model/OAuth2Access.scala
+++ data-provider/src/main/scala/velocorner/model/OAuth2Access.scala
@@ -4,8 +4,8 @@ import org.joda.time.DateTime
 import play.api.libs.json.{Format, Json}
 
 object OAuth2Access {
-  implicit val dateTimeFormat = DateTimePattern.createLongFormatter
-  implicit val dataFormat = Format[OAuth2Access](Json.reads[OAuth2Access], Json.writes[OAuth2Access])
+  implicit val dateTimeFormat: Format[DateTime] = DateTimePattern.createLongFormatter
+  implicit val dataFormat: Format[OAuth2Access] = Format[OAuth2Access](Json.reads[OAuth2Access], Json.writes[OAuth2Access])
 }
 
 case class OAuth2Access(
--- web-app/app/velocorner/api/wordcloud/WordCloud.scala
+++ web-app/app/velocorner/api/wordcloud/WordCloud.scala
@@ -3,7 +3,7 @@ package velocorner.api.wordcloud
 import play.api.libs.json.{Format, Json, Reads}
 
 object WordCloud {
-  implicit val cloudFormat = Format[WordCloud](Json.reads[WordCloud], Json.writes[WordCloud])
+  implicit val cloudFormat: Format[WordCloud] = Format[WordCloud](Json.reads[WordCloud], Json.writes[WordCloud])
   implicit val listCloud: Reads[List[WordCloud]] = Reads.list(cloudFormat)
 }
 
