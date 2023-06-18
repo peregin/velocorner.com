e83b3b1b23f6f448d21111bd118f6ed44ebce052
Scala Steward <scala_steward@virtuslab.com>
2023-06-18 16:35:40 +0000

    Update mockito-core to 5.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   val scalacacheVersion = "0.28.0"
   val jsoupVersion = "1.16.1"
   val scalaTestVersion = "3.2.16"
-  val mockitoVersion = "5.3.1"
+  val mockitoVersion = "5.4.0"
   val catsEffectTestVersion = "1.5.0"
 
   val springVersion = "3.1.0" // java world
