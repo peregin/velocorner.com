c9febf57f60f7f8228cf8229cd82a1356232dea9
Scala Steward <scala_steward@virtuslab.com>
2023-09-18 19:38:30 +0000

    Update play-ahc-ws-standalone, ... to 2.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val zioVersion = "2.0.17"
   val finatraVersion = "21.2.0" // in the test service
   val argonautVersion = "6.3.8" // in the test service
-  val playWsVersion = "2.2.0" // standalone version
+  val playWsVersion = "2.2.2" // standalone version
   val sangriaVersion = "4.0.2"
   val playJsonVersion = "2.10.0"
   val shapelessVersion = "2.3.10"
