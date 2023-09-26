f9475835f4451201a38050a2a165161b57fee05c
Scala Steward <scala_steward@virtuslab.com>
2023-09-26 17:26:27 +0000

    Update zio to 2.0.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val mouseVersion = "1.2.1"
   val fs2Version = "3.9.2"
   val http4s = "0.23.23"
-  val zioVersion = "2.0.17"
+  val zioVersion = "2.0.18"
   val finatraVersion = "21.2.0" // in the test service
   val argonautVersion = "6.3.8" // in the test service
   val playWsVersion = "2.2.0" // standalone version
