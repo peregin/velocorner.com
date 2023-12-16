4212757e846a92bfccd67bdbb1aa7d412d3afb70
levi <velocorner.com@gmail.com>
2023-12-16 17:28:31 +0100

    bump sbt and libs

--- data-provider/src/test/scala/velocorner/util/JsonIoSpec.scala
+++ data-provider/src/test/scala/velocorner/util/JsonIoSpec.scala
@@ -10,11 +10,11 @@ import scala.io.Source
 /**
  * Created by levi on 08/02/15.
  */
-class JsonIoSpec extends AnyWordSpec with Matchers {
+class JsonIoSpec extends AnyWordSpec with Matchers with CloseableResource {
 
   "converter for athlete activities" should {
 
-    val json = Source.fromURL(getClass.getResource("/data/strava/last30activities.json")).mkString
+    val json = withCloseable(Source.fromURL(getClass.getResource("/data/strava/last30activities.json")))(_.mkString)
 
     "read Strava activity" in {
       val list = JsonIo.read[List[Activity]](json)
@@ -35,7 +35,7 @@ class JsonIoSpec extends AnyWordSpec with Matchers {
     }
 
     "read all the activity details" in {
-      val jsonSingle = Source.fromURL(getClass.getResource("/data/strava/single_activity.json")).mkString
+      val jsonSingle = withCloseable(Source.fromURL(getClass.getResource("/data/strava/single_activity.json")))(_.mkString)
       val activity = JsonIo.read[Activity](jsonSingle)
       activity.max_speed mustBe Some(13.6f)
       activity.average_speed mustBe Some(4.732f)
@@ -44,7 +44,7 @@ class JsonIoSpec extends AnyWordSpec with Matchers {
   }
 
   "converter for club activities" should {
-    val json = Source.fromURL(getClass.getResource("/data/strava/club_activity.json")).mkString
+    val json = withCloseable(Source.fromURL(getClass.getResource("/data/strava/club_activity.json")))(_.mkString)
 
     "read the json file" in {
       val list = JsonIo.read[List[Activity]](json)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,25 +2,25 @@ object Dependencies {
 
   val projectScalaVersion = "2.13.12"
 
-  val flywayVersion = "10.2.0"
+  val flywayVersion = "10.3.0"
   val catsVersion = "2.10.0"
   val catsEffectVersion = "3.5.2"
   val mouseVersion = "1.2.2"
   val fs2Version = "3.9.3"
   val http4s = "0.23.24"
-  val zioVersion = "2.0.19"
+  val zioVersion = "2.0.20"
   val finatraVersion = "21.2.0" // in the test service
   val argonautVersion = "6.3.9" // in the test service
-  val playWsVersion = "3.0.0" // standalone version
+  val playWsVersion = "3.0.1" // standalone version
   val sangriaVersion = "4.0.2"
   val playJsonVersion = "3.0.1"
   val shapelessVersion = "2.3.10"
   val logbackVersion = "1.4.14" // updating will cause conflict
   val doobieVersion = "1.0.0-RC5"
-  val orientDbVersion = "3.2.24"
+  val orientDbVersion = "3.2.25"
   val mongoDbVersion = "4.11.1"
   val rethinkDbVersion = "2.4.4"
-  val elasticVersion = "8.11.3"
+  val elasticVersion = "8.11.4"
   val jwtVersion = "9.4.5"
   val squantsVersion = "1.8.3"
   val sparkVersion = "3.5.0"
