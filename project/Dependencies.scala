b2ca22d64f59d998edcda111b7e554ea0e026514
Scala Steward <scala_steward@virtuslab.com>
2024-08-13 01:02:34 +0000

    Update mongo-scala-driver to 5.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val logbackVersion = "1.5.6" // updating will cause conflict
   val doobieVersion = "1.0.0-RC5"
   val orientDbVersion = "3.2.31"
-  val mongoDbVersion = "5.1.2"
+  val mongoDbVersion = "5.1.3"
   val rethinkDbVersion = "2.4.4"
   val elasticVersion = "8.14.0"
   val jwtVersion = "10.0.1"
