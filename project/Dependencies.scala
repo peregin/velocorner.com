2613ec6abcdbadda7f4114edc3e807d01818c510
levi <velocorner.com@gmail.com>
2024-09-20 13:41:29 +0200

    bump deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,10 +18,10 @@ object Dependencies {
   val shapelessVersion = "2.3.12"
   val logbackVersion = "1.5.8" // updating will cause conflict
   val doobieVersion = "1.0.0-RC6"
-  val orientDbVersion = "3.2.31"
+  val orientDbVersion = "3.2.33"
   val mongoDbVersion = "5.1.4"
   val rethinkDbVersion = "2.4.4"
-  val elasticVersion = "8.15.0"
+  val elasticVersion = "8.15.1"
   val jwtVersion = "10.0.1"
   val squantsVersion = "1.8.3"
   val sparkVersion = "3.5.3"
@@ -32,7 +32,7 @@ object Dependencies {
   val mockitoVersion = "5.13.0"
   val catsEffectTestVersion = "1.5.0"
 
-  val springVersion = "3.3.3" // java world
+  val springVersion = "3.3.4" // java world
 }
 
 object DockerBuild {
