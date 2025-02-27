00e0c57fdcf96a45fd701ea4bd983b936f39feb2
Scala Steward <scala_steward@virtuslab.com>
2025-02-27 16:13:28 +0000

    Update doobie-core, doobie-hikari, ... to 1.0.0-RC8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   val playJsonVersion = "3.0.4"
   val shapelessVersion = "2.3.12"
   val logbackVersion = "1.5.17" // updating will cause conflict
-  val doobieVersion = "1.0.0-RC7"
+  val doobieVersion = "1.0.0-RC8"
   val orientDbVersion = "3.2.37"
   val mongoDbVersion = "5.3.1"
   val rethinkDbVersion = "2.4.4"
