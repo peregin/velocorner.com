e874db08b1ed3bab4fa6bd1677ac6adacba19da9
Scala Steward <scala_steward@virtuslab.com>
2025-06-10 16:30:25 +0000

    Update elastic4s-client-esjava, ... to 8.18.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val orientDbVersion = "3.2.39"
   val mongoDbVersion = "5.5.0"
   val rethinkDbVersion = "2.4.4"
-  val elasticVersion = "8.18.1"
+  val elasticVersion = "8.18.2"
   val jwtVersion = "10.0.4"
   val squantsVersion = "1.8.3"
   val sparkVersion = "3.5.6"
