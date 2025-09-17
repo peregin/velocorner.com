dbf36bea5e277d9276d2da52b0745ebedad9c699
velocorner <velocorner.com@gmail.com>
2025-09-17 23:29:30 +0200

    remove strict mode [skip ci]

--- web-frontend/src/index.jsx
+++ web-frontend/src/index.jsx
@@ -7,9 +7,7 @@ import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider>
+    <App />
+  </Provider>
 );
