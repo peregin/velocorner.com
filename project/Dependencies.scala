56b2c083c5c51d758aa363483d6d7d08acf0c1b6
Scala Steward <scala_steward@virtuslab.com>
2025-10-31 17:18:49 +0000

    Update http4s-circe, http4s-dsl, ... to 0.23.33

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val catsEffectVersion = "3.6.3"
   val mouseVersion = "1.4.0"
   val fs2Version = "3.12.2"
-  val http4s = "0.23.32"
+  val http4s = "0.23.33"
   val zioVersion = "2.1.21"
   val playWsVersion = "3.0.8" // standalone version
   val pekkoVersion = "1.2.1"
