e329b3e904834f1e81975ee979e1432a517ea1b1
Scala Steward <scala_steward@virtuslab.com>
2025-11-12 18:11:42 +0000

    Update flyway-core, ... to 11.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Dependencies {
 
   val projectScalaVersion = "2.13.17"
 
-  val flywayVersion = "11.15.0"
+  val flywayVersion = "11.17.0"
   val catsVersion = "2.13.0"
   val catsEffectVersion = "3.6.3"
   val mouseVersion = "1.4.0"
