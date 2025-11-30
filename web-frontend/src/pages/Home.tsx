8c8efa908e5679258fd7bc33797273d51e7e2bab
velocorner <velocorner.com@gmail.com>
2025-11-30 19:35:12 +0100

    fix iframe warning

--- web-frontend/src/pages/Home.tsx
+++ web-frontend/src/pages/Home.tsx
@@ -425,7 +425,11 @@ const Home = () => {
       </VStack>
 
       <Box py='1rem'>
-        <iframe width="100%" height="400" src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface&lat=47.31&lon=8.527&detailLat=47.31&detailLon=8.527000000000044&marker=true" frameborder="0"></iframe>
+        <iframe 
+          width="100%" height="400" 
+          src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface&lat=47.31&lon=8.527&detailLat=47.31&detailLon=8.527000000000044&marker=true" 
+          title="Windy Map" style={{ border: 'none' }}>
+        </iframe>
       </Box>
     </Box>
   );
