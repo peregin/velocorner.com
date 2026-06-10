c05e7010748501bd24f8ba5f52f97a21e5f0f92e
Scala Steward <scala_steward@virtuslab.com>
2026-06-10 17:28:47 +0000

    Update tapir-http4s-server, ... to 1.13.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val catsEffectVersion = "3.7.0"
   val mouseVersion = "1.4.0"
   val http4s = "0.23.34"
-  val tapirVersion = "1.13.18"
+  val tapirVersion = "1.13.20"
   val playWsVersion = "3.0.10" // standalone version
   val pekkoVersion = "1.6.0"
   val playJsonVersion = "3.0.6"
