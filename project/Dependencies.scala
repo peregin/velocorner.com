accb9663522a727a363da4911f2857719462db8f
Scala Steward <scala_steward@virtuslab.com>
2026-08-27 20:17:10 +0000

    Update jsoup to 1.23.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val sparkVersion = "4.2.0"
   val circeVersion = "0.14.16"
   val scalacacheVersion = "0.28.0"
-  val jsoupVersion = "1.23.1"
+  val jsoupVersion = "1.23.2"
   val scalaTestVersion = "3.2.20"
   val mockitoVersion = "5.23.0"
   val catsEffectTestVersion = "1.8.0"
